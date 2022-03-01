c6fdd38dce21cd69815088a5aa3499459869d621
namonak <namonak@gmail.com>
2022-03-01 13:38:39 +0900

    버블 정렬 구현 및 테스트 코드 추가

---
+++ src/main/kotlin/ds/sorting/BubbleSorting.kt
@@ -0,0 +1,16 @@
+package ds.sorting
+
+class BubbleSorting {
+    fun sort(array: IntArray) : IntArray {
+        for (i in array.indices) {
+            for (j in 1 until array.size - i) {
+                if (array[j - 1] > array[j]) {
+                    val temp = array[j]
+                    array[j] = array[j - 1]
+                    array[j - 1] = temp
+                }
+            }
+        }
+        return array
+    }
+}
---
+++ src/test/kotlin/ds/sorting/TestBubleSorting.kt
@@ -0,0 +1,28 @@
+package ds.sorting
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+data class TestCase(val given: IntArray, val want: IntArray)
+
+class TestBubbleSorting {
+    private val mTestBubbleSorting = arrayOf(
+        TestCase(
+            intArrayOf(9, 3, 5, 7, 1),
+            intArrayOf(1, 3, 5, 7, 9),
+        )
+    )
+
+    @Test
+    fun testSort() {
+        val bubbleSorting = BubbleSorting()
+
+        mTestBubbleSorting.forEach {
+            val result = bubbleSorting.sort(it.given)
+
+            for (i in it.want.indices) {
+                assertEquals(it.want[i], result[i])
+            }
+        }
+    }
+}
