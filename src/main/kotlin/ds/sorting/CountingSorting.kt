0986469de070ed8288241cc946e8b4d704b85018
namonak <namonak@gmail.com>
2022-03-09 23:33:57 +0900

    카운팅 정렬 구현 및 테스트 코드 추가

---
+++ src/main/kotlin/ds/sorting/CountingSorting.kt
@@ -0,0 +1,23 @@
+package ds.sorting
+
+class CountingSorting {
+    fun sort(array: IntArray) : IntArray {
+        val n = array.size
+        val count = IntArray(n)
+        for (i in 0 until n) {
+            count[i] = 0
+        }
+        for (i in 0 until n) {
+            count[array[i]]++
+        }
+        for (i in 1 until n) {
+            count[i] += count[i - 1]
+        }
+        val sorted = IntArray(n)
+        for (i in n - 1 downTo 0) {
+            sorted[count[array[i]] - 1] = array[i]
+            count[array[i]]--
+        }
+        return sorted
+    }
+}
---
+++ src/test/kotlin/ds/sorting/TestCountingSorting.kt
@@ -0,0 +1,32 @@
+package ds.sorting
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+data class CountingSortingTestCase(val given: IntArray, val want: IntArray)
+
+class TestCountingSorting {
+    private val mCountingSortingTestCase = arrayOf(
+        CountingSortingTestCase(
+            intArrayOf(3, 3, 2, 1, 2),
+            intArrayOf(1, 2, 2, 3, 3)
+        ),
+        CountingSortingTestCase(
+            intArrayOf(3, 4, 0, 1, 2),
+            intArrayOf(0, 1, 2, 3, 4)
+        )
+    )
+
+    @Test
+    fun testSort() {
+        val countingSorting = CountingSorting()
+
+        mCountingSortingTestCase.forEach {
+            val result = countingSorting.sort(it.given)
+
+            for (i in it.want.indices) {
+                assertEquals(it.want[i], result[i])
+            }
+        }
+    }
+}
