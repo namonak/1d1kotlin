bd5f7bc69875b355319c36fae4b9fb0dcfe4c24b
namonak <namonak@gmail.com>
2022-03-14 23:17:39 +0900

    LeetCode : Merge Two Sorted Lists

---
+++ src/main/kotlin/leetcode/problems/lv0easy/MergeTwoSortedLists.kt
@@ -0,0 +1,33 @@
+package leetcode.problems.lv0easy
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class MergeTwoSortedLists {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        var l1 = list1
+        var l2 = list2
+        val head = ListNode(0)
+        var cur = head
+
+        while (l1 != null && l2 != null) {
+            if (l1.`val` < l2.`val`) {
+                cur.next = l1
+                l1 = l1.next
+            } else {
+                cur.next = l2
+                l2 = l2.next
+            }
+            cur = cur.next!!
+        }
+
+        if (l1 != null) {
+            cur.next = l1
+        } else if (l2 != null) {
+            cur.next = l2
+        }
+
+        return head.next
+    }
+}
---
+++ src/test/kotlin/leetcode/problems/lv0easy/TestMergeTwoSortedLists.kt
@@ -0,0 +1,71 @@
+package leetcode.problems.lv0easy
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+data class MergeTwoSortedListsTestCase(val given0: ListNode?, val given1: ListNode?, val want: ListNode?)
+
+fun buildListNode(arr: ArrayList<Int>): ListNode? {
+    val head: ListNode? = ListNode(0)
+    var cur = head
+    for (n in arr) {
+        cur?.next = ListNode(n)
+        cur = cur?.next
+    }
+    return head?.next
+}
+
+fun nodeToIntArray(node: ListNode?): MutableList<Int> {
+    val res = mutableListOf<Int>()
+    var cur = node
+    while(cur != null) {
+        res.add(cur.`val`)
+        cur = cur.next
+    }
+    return res
+}
+
+class TestMergeTwoSortedLists {
+    private val mTestData = arrayOf(
+        MergeTwoSortedListsTestCase(
+            given0 = buildListNode(arrayListOf(1, 2, 4)),
+            given1 = buildListNode(arrayListOf(1, 3, 4)),
+            want = buildListNode(arrayListOf(1, 1, 2, 3, 4, 4))
+        ),
+        MergeTwoSortedListsTestCase(
+            given0 = null,
+            given1 = buildListNode(arrayListOf(0)),
+            want = buildListNode(arrayListOf(0))
+        ),
+        MergeTwoSortedListsTestCase(
+            given0 = null,
+            given1 = null,
+            want = null,
+        ),
+        MergeTwoSortedListsTestCase(
+            given0 = buildListNode(arrayListOf(1, 8, 9, 10)),
+            given1 = buildListNode(arrayListOf(3, 4)),
+            want = buildListNode(arrayListOf(1, 3, 4, 8, 9, 10))
+        ),
+        MergeTwoSortedListsTestCase(
+            given0 = buildListNode(arrayListOf(3, 4)),
+            given1 = buildListNode(arrayListOf(1, 8, 9, 10)),
+            want = buildListNode(arrayListOf(1, 3, 4, 8, 9, 10))
+        ),
+    )
+
+    @Test
+    fun testMergeTwoSortedLists() {
+        val mergeTwoLists = MergeTwoSortedLists()
+        for (testCase in mTestData) {
+            val got = mergeTwoLists.mergeTwoLists(testCase.given0, testCase.given1)
+            for (i in 0 until nodeToIntArray(got).size) {
+                assertEquals(nodeToIntArray(got)[i], nodeToIntArray(testCase.want)[i])
+            }
+        }
+    }
+}
