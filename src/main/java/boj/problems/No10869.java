6360f353a17d2a1d750e2b4c08ce6f0d9bc293fc
namonak <namonak@gmail.com>
2022-05-25 23:41:57 +0900

    feat : BOJ, '사칙연산' 구현부 추가
     - https://www.acmicpc.net/problem/10869

---
+++ src/main/java/boj/problems/No10869.java
@@ -0,0 +1,32 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        output.write(a + b + "\n");
+        output.write(a - b + "\n");
+        output.write(a * b + "\n");
+        output.write(a / b + "\n");
+        output.write(a % b + "\n");
+    }
+}
