0ec6e1e0ee324744004e256f738b8c915ed88370
namonak <namonak@gmail.com>
2022-06-04 12:36:02 +0900

    test : BOJ, 'A-B' 테스트 코드 추가
     - https://www.acmicpc.net/problem/1001

---
+++ src/test/java/boj/problems/No1001Test.java
@@ -0,0 +1,37 @@
+package boj.problems;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class No1001Test {
+    @Test
+    @DisplayName("A-B 테스트")
+    void A_빼기_B_테스트() throws IOException {
+        System.out.println("A+B : https://www.acmicpc.net/problem/1001");
+
+        BufferedReader br_given = new BufferedReader(new StringReader("3 2"));
+        StringWriter sw = new StringWriter();
+        BufferedWriter bw_got = new BufferedWriter(sw);
+
+        No1001.solve(br_given, bw_got);
+
+        bw_got.close();
+
+        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
+
+        int got = Integer.parseInt(br_got.lines().collect(Collectors.joining()));
+
+        assertEquals(1, got);
+    }
+}
