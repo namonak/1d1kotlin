815c56ce25864b09c4edd3da39713e28a4bb8b1e
namonak <namonak@gmail.com>
2022-06-06 01:03:33 +0900

    refactor : BOJ, '피보나치 함수' 구현체 리팩토링
     - https://www.acmicpc.net/problem/1003

--- src/main/java/boj/problems/No1003.java
+++ src/main/java/boj/problems/No1003.java
@@ -8,8 +8,6 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 class No1003 {
-    static Integer[][] dp = new Integer[41][2];
-
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -20,27 +18,30 @@ class No1003 {
         output.close();
     }
 
-    private static Integer[] fibonacci(int N) {
-        if(dp[N][0] == null || dp[N][1] == null) {
-            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-        }
-        return dp[N];
+    private static Integer[] fibonacci(int number) {
+        int[] fib0 = new int[number + 1];
+        int[] fib1 = new int[number + 1];
 
+        fib0[0] = 1;
+        fib0[1] = 0;
+        fib1[0] = 0;
+        fib1[1] = 1;
+
+        for (int i = 2; i <= number; i++) {
+            fib0[i] = fib0[i - 1] + fib0[i - 2];
+            fib1[i] = fib1[i - 1] + fib1[i - 2];
+        }
+
+        return new Integer[] { fib0[number], fib1[number] };
     }
 
     static void solve(BufferedReader input, BufferedWriter output) throws IOException {
-        int T = Integer.parseInt(input.readLine());
+        int testCase = Integer.parseInt(input.readLine());
 
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-
-        while(T-- > 0){
+        for (int i = 0; i < testCase; i++) {
             StringTokenizer st = new StringTokenizer(input.readLine());
             Integer[] result = fibonacci(Integer.parseInt(st.nextToken()));
-            output.write(result[0] + result[1] + "\n");
+            output.write(result[0] + " " + result[1] + "\n");
         }
     }
 }
