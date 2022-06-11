d71a361b765e5e92229947e24522c9be09c51c20
namonak <namonak@gmail.com>
2022-06-11 21:51:19 +0900

    test : BOJ, '최소, 최대'. 테스트 코드 및 테스트 케이스 추가
     - https://www.acmicpc.net/problem/10818

---
+++ src/test/java/boj/problems/No10818Test.java
@@ -0,0 +1,37 @@
+package boj.problems;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class No10818Test {
+    File path = new File(".");
+    String input = path.getAbsolutePath() + "/src/test/java/boj/problems/No10818_input.txt";
+    String output = path.getAbsolutePath() + "/src/test/java/boj/problems/No10818_output.txt";
+
+    @Test
+    @DisplayName("최소, 최대")
+    void 최소_최대_테스트() throws IOException {
+        System.out.println("최소, 최대 : https://www.acmicpc.net/problem/10818");
+
+        BufferedReader br_given = new BufferedReader(new FileReader(input));
+        BufferedReader br_want = new BufferedReader(new FileReader(output));
+        StringWriter sw = new StringWriter();
+        BufferedWriter bw_got = new BufferedWriter(sw);
+
+        No10818.solve(br_given, bw_got);
+
+        bw_got.close();
+
+        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
+
+        String got = br_got.lines().collect(Collectors.joining());
+        String want = br_want.lines().collect(Collectors.joining());
+
+        assertEquals(got, want);
+    }
+}
