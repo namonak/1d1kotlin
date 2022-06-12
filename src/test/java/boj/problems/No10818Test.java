38e85c110871703537d890bdaae0c60dadfb5a64
namonak <namonak@gmail.com>
2022-06-12 22:56:07 +0900

    refactor : BOJ, '최소, 최대' 테스트 코드 리팩토링
     - https://www.acmicpc.net/problem/10818

--- src/test/java/boj/problems/No10818Test.java
+++ src/test/java/boj/problems/No10818Test.java
@@ -32,6 +32,6 @@ public class No10818Test {
         String got = br_got.lines().collect(Collectors.joining());
         String want = br_want.lines().collect(Collectors.joining());
 
-        assertEquals(got, want);
+        assertEquals(want, got);
     }
 }
