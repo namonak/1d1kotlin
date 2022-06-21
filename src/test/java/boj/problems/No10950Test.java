71badbba8dc281a53916dbce6603cd5bb729739d
namonak <namonak@gmail.com>
2022-06-21 23:18:42 +0900

    test : BOJ, 'A+B - 3' 테스트 코드 및 테스트 케이스 추가
     - https://www.acmicpc.net/problem/10950

---
+++ src/test/java/boj/problems/No10950Test.java
@@ -0,0 +1,43 @@
+package boj.problems;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class No10950Test {
+    File path = new File(".");
+    String input = path.getAbsolutePath() + "/src/test/java/boj/problems/No10950_input.txt";
+    String output = path.getAbsolutePath() + "/src/test/java/boj/problems/No10950_output.txt";
+
+    @Test
+    @DisplayName("A+B - 3")
+    void A_더하기_B_다시_3_테스트() throws IOException {
+        System.out.println("A+B - 3 : https://www.acmicpc.net/problem/10950");
+
+        BufferedReader br_given = new BufferedReader(new FileReader(input));
+        BufferedReader br_want = new BufferedReader(new FileReader(output));
+        StringWriter sw = new StringWriter();
+        BufferedWriter bw_got = new BufferedWriter(sw);
+
+        No10950.solve(br_given, bw_got);
+
+        bw_got.close();
+
+        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
+
+        String got = br_got.lines().collect(Collectors.joining());
+        String want = br_want.lines().collect(Collectors.joining());
+
+        assertEquals(want, got);
+    }
+}
