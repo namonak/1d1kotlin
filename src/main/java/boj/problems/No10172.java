211e6821f06c165737d26cd38cf81bea781f222e
namonak <namonak@gmail.com>
2022-06-23 22:51:39 +0900

    feat : BOJ, '개' 구현체 추가
     - https://www.acmicpc.net/problem/10172

---
+++ src/main/java/boj/problems/No10172.java
@@ -0,0 +1,15 @@
+package boj.problems;
+
+public class No10172 {
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.println(sb);
+    }
+}
