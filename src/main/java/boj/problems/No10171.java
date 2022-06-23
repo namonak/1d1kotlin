082cf23e3ecd6f43cf5395d3981c5caf0363813d
namonak <namonak@gmail.com>
2022-06-23 22:53:15 +0900

    feat : BOJ, '고양이' 구현체 추가
     - https://www.acmicpc.net/problem/10171

---
+++ src/main/java/boj/problems/No10171.java
@@ -0,0 +1,14 @@
+package boj.problems;
+
+public class No10171 {
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+
+        System.out.println(sb);
+    }
+}
