2b856ef3e94f16acc7a7cd8d2291da4324ca9fcd
namonak <namonak@gmail.com>
2022-06-23 23:05:13 +0900

    feat : BOJ, '새싹' 구현체 추가
     - https://www.acmicpc.net/problem/25083

---
+++ src/main/java/boj/problems/No25083.java
@@ -0,0 +1,16 @@
+package boj.problems;
+
+public class No25083 {
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |\n");
+
+        System.out.println(sb);
+    }
+}
