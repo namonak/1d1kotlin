783c2209c908d677d8b3464c27f2186f8e74c950
namonak <namonak@gmail.com>
2022-06-25 23:24:22 +0900

    feat : BOJ, '구구단' 구현체 추가
     - https://www.acmicpc.net/problem/2739

---
+++ src/main/java/boj/problems/No2739.java
@@ -0,0 +1,27 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int number = Integer.parseInt(input.readLine());
+
+        for (int i = 1; i <= 9; i++) {
+            output.write(number + " * " + i + " = " + (number * i) + "\n");
+        }
+    }
+}
