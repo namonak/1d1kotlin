fed870721cdfb129777334578edc45d206936096
namonak <namonak@gmail.com>
2022-06-30 22:54:14 +0900

    test : BOJ, '최댓값' 테스트 코드 수정
     - https://www.acmicpc.net/problem/2562

--- src/test/java/boj/problems/No2562Test.java
+++ src/test/java/boj/problems/No2562Test.java
@@ -29,7 +29,7 @@ public class No2562Test {
         StringWriter sw = new StringWriter();
         BufferedWriter bw_got = new BufferedWriter(sw);
 
-        No1003.solve(br_given, bw_got);
+        No2562.solve(br_given, bw_got);
 
         bw_got.close();
 
