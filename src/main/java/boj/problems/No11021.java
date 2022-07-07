9edea97a3f96bff7dd8431cedec25ee49587458a
namonak <namonak@gmail.com>
2022-07-07 23:27:39 +0900

    feat : BOJ, 'A+B - 7' 구현부 추가
     - https://www.acmicpc.net/problem/11021

---
+++ src/main/java/boj/problems/No11021.java
@@ -0,0 +1,33 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int testCase = Integer.parseInt(input.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(input.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            output.write("Case #" + (i + 1) + ": " + (a + b) + "\n");
+        }
+    }
+}
