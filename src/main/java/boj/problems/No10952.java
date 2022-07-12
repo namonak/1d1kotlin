f2653f31e2a7b90b2703c0c2a9d059d00fb71b4f
namonak <namonak@gmail.com>
2022-07-12 23:28:22 +0900

    feat : BOJ, 'A+B - 5' 구현부 추가
    - https://www.acmicpc.net/problem/10952

---
+++ src/main/java/boj/problems/No10952.java
@@ -0,0 +1,35 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        while (true) {
+            StringTokenizer st = new StringTokenizer(input.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            output.write((a + b) + "\n");
+        }
+    }
+}
