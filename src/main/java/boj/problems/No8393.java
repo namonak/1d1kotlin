1bc9d7a33058d56d0dd0d4c9794ebe508e432f8b
namonak <namonak@gmail.com>
2022-07-13 22:36:53 +0900

    feat : BOJ, '합' 구현부 추가
     - https://www.acmicpc.net/problem/8393

---
+++ src/main/java/boj/problems/No8393.java
@@ -0,0 +1,30 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No8393 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int n = Integer.parseInt(input.readLine());
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        output.write(String.valueOf(sum));
+    }
+}
