20d27b5f2b92aa466995c91616d892d54f37a5e3
namonak <namonak@gmail.com>
2022-07-21 00:24:12 +0900

    feat : BOJ, '별 찍기 - 1' 구현부 추가
     - https://www.acmicpc.net/problem/2438

---
+++ src/main/java/boj/problems/No2438.java
@@ -0,0 +1,30 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int n = Integer.parseInt(input.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                output.write("*");
+            }
+            output.write("\n");
+        }
+    }
+}
