96aa5b55d4371117fd09f7d7cc18e96cbc37fdb3
namonak <namonak@gmail.com>
2022-07-22 23:08:41 +0900

    feat : BOJ, '상수' 구현부 추가
     - https://www.acmicpc.net/problem/2908

---
+++ src/main/java/boj/problems/No2908.java
@@ -0,0 +1,35 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        StringTokenizer st = new StringTokenizer(input.readLine());
+
+        int a = reverseNumber(st.nextToken());
+        int b = reverseNumber(st.nextToken());
+
+        output.write((a > b ? a : b) + "\n");
+    }
+
+    private static int reverseNumber(String str) {
+        StringBuffer sb = new StringBuffer(str);
+
+        return Integer.parseInt(sb.reverse().toString());
+    }
+}
