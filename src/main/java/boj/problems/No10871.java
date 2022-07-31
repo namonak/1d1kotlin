cbe63ddf16104f094bc02c3b6aba249204d54963
namonak <namonak@gmail.com>
2022-07-31 22:18:30 +0900

    feat : BOJ, 'X보다 작은 수' 구현부 추가
     - https://www.acmicpc.net/problem/10871

---
+++ src/main/java/boj/problems/No10871.java
@@ -0,0 +1,39 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int testCase = Integer.parseInt(st.nextToken());
+        int number = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(input.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(st.nextToken());
+
+            if (n < number) {
+                sb.append(n + " ");
+            }
+        }
+
+        output.write(String.valueOf(sb));
+    }
+}
