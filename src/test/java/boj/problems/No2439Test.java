b76c3e85fbcb607ed27ce454f114fc8abbd7d9af
namonak <namonak@gmail.com>
2022-08-04 16:11:06 +0900

    test : BOJ, '별 찍기 - 2' 테스트 코드 및 테스트 케이스 추가
     - https://www.acmicpc.net/problem/2439

---
+++ src/test/java/boj/problems/No2439Test.java
@@ -0,0 +1,43 @@
+package boj.problems;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class No2439Test {
+    File path = new File(".");
+    String input = path.getAbsolutePath() + "/src/test/java/boj/problems/No2439_input.txt";
+    String output = path.getAbsolutePath() + "/src/test/java/boj/problems/No2439_output.txt";
+
+    @Test
+    @DisplayName("별 찍기 - 2 테스트")
+    void 별_찍기_빼기_2_테스트() throws IOException {
+        System.out.println("별 찍기 - 2 : https://www.acmicpc.net/problem/2439");
+
+        BufferedReader br_given = new BufferedReader(new FileReader(input));
+        BufferedReader br_want = new BufferedReader(new FileReader(output));
+        StringWriter sw = new StringWriter();
+        BufferedWriter bw_got = new BufferedWriter(sw);
+
+        No2439.solve(br_given, bw_got);
+
+        bw_got.close();
+
+        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
+
+        String got = br_got.lines().collect(Collectors.joining());
+        String want = br_want.lines().collect(Collectors.joining());
+
+        assertEquals(got, want);
+    }
+}
