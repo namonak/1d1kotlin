73481d98a64d77f85c19487a33ebe195162c8ef2
namonak <namonak@gmail.com>
2022-08-06 12:16:43 +0900

    feat : BOJ, '더하기 사이클' 구현부 추가
     - https://www.acmicpc.net/problem/1110

---
+++ src/main/java/boj/problems/No1110.java
@@ -0,0 +1,34 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int number = Integer.parseInt(input.readLine());
+        int result = 0;
+        int newNumber = number;
+
+        do {
+            int leftNumber = newNumber / 10;
+            int rightNumber = newNumber % 10;
+            newNumber = (rightNumber * 10) + ((leftNumber + rightNumber) % 10);
+            result++;
+        } while(number != newNumber);
+
+        output.write(result + "\n");
+    }
+}
