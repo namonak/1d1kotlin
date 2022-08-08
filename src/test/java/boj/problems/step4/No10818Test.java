f2b2e1aab234e3c6eb54ea5e2e388f696428fe47
namonak <namonak@gmail.com>
2022-08-09 00:09:09 +0900

    test : BOJ, '최소, 최대' 테스트 코드 수정
     - https://www.acmicpc.net/problem/10818

--- src/test/java/boj/problems/step4/No10818Test.java
+++ src/test/java/boj/problems/step4/No10818Test.java
@@ -16,8 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class No10818Test {
     File path = new File(".");
-    String input = path.getAbsolutePath() + "/src/test/java/boj/problems/No10818_input.txt";
-    String output = path.getAbsolutePath() + "/src/test/java/boj/problems/No10818_output.txt";
+    String input = path.getAbsolutePath() + "/src/test/java/boj/problems/step4/No10818_input.txt";
+    String output = path.getAbsolutePath() + "/src/test/java/boj/problems/step4/No10818_output.txt";
 
     @Test
     @DisplayName("최소, 최대")
