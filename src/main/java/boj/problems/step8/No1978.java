91eacddeb8e5e70a2097a028726e620f0c40c1e7
namonak <namonak@gmail.com>
2022-08-09 22:57:50 +0900

    feat : BOJ, '소수 찾기' 구현부 추가
     - https://www.acmicpc.net/problem/1978

---
+++ src/main/java/boj/problems/step8/No1978.java
@@ -0,0 +1,46 @@
+package boj.problems.step8;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int result = 0;
+        // N은 사용하지 않음
+        input.readLine();
+        StringTokenizer st = new StringTokenizer(input.readLine(), " ");
+
+        while (st.hasMoreTokens()) {
+            if (isPrime(Integer.parseInt(st.nextToken()))) {
+                result++;
+            }
+        }
+
+        output.write(result + "\n");
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int div = 2; div < number; div++) {
+            if (number % div == 0) return false;
+        }
+        return true;
+    }
+}
