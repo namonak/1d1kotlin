e1362fb46b978c8b2d3f71cd54b4825d7e915c76
namonak <namonak@gmail.com>
2022-09-06 23:10:22 +0900

    feat : BOJ, '큰 수 A+B' 구현부 추가
     - https://www.acmicpc.net/problem/10757

---
+++ src/main/java/boj/problems/step7/No10757.java
@@ -0,0 +1,30 @@
+package boj.problems.step7;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class No10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        input.close();
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        output.write(a.add(b) + "\n");
+    }
+}
