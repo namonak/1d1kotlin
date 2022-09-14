3c9a74456702ce2c9549bee17f19420a535eb4f1
namonak <namonak@gmail.com>
2022-09-14 23:16:24 +0900

    feat : BOJ, '좌표 정렬하기' 구현부 추가
     - https://www.acmicpc.net/problem/11650

---
+++ src/main/java/boj/problems/step9/No11650.java
@@ -0,0 +1,46 @@
+package boj.problems.step9;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class No11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve(input, output);
+
+        input.close();
+        output.flush();
+        output.close();
+    }
+
+    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+        int N = Integer.parseInt(input.readLine());
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for(int i = 0; i < N; i++) {
+            output.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+    }
+}
