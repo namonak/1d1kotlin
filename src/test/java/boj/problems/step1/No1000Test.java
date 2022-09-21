cbc1d2d7347c723ae596d07ab54bd86348577a78
namonak <namonak@gmail.com>
2022-09-21 23:37:37 +0900

    refactor(test): test code refactoring
    Changed the following:
     - change BufferedReader/BufferedWriter to @ParameterizedTest
     - apply CsvSource to input/output values

--- src/test/java/boj/problems/step1/No1000Test.java
+++ src/test/java/boj/problems/step1/No1000Test.java
@@ -1,35 +1,24 @@
 package boj.problems.step1;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class No1000Test {
-    @Test
+    @ParameterizedTest
     @DisplayName("A+B 테스트")
-    void A_더하기_B_테스트() throws IOException {
+    @CsvSource(value = {"1 2:3", "5 5:10", "100 101:201", "1 7:8", "12 29:41"}, delimiter = ':')
+    void test(String input, String expected) throws IOException {
         System.out.println("A+B : https://www.acmicpc.net/problem/1000");
 
-        BufferedReader br_given = new BufferedReader(new StringReader("1 2"));
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw_got = new BufferedWriter(sw);
+        BufferedReader br_given = new BufferedReader(new StringReader(input));
 
-        No1000.solve(br_given, bw_got);
-
-        bw_got.close();
-
-        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-        int got = Integer.parseInt(br_got.lines().collect(Collectors.joining()));
-
-        assertEquals(3, got);
+        assertEquals(expected, String.valueOf(No1000.solve(br_given)));
     }
 }
