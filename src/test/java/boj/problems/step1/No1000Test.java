d7683ecb2cc8b48f2468d3735605ff3cb29dfcb0
namonak <namonak@gmail.com>
2022-09-24 22:59:12 +0900

    refactor(test): update test code

--- src/test/java/boj/problems/step1/No1000Test.java
+++ src/test/java/boj/problems/step1/No1000Test.java
@@ -11,13 +11,14 @@ import java.io.StringReader;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class No1000Test {
-    @ParameterizedTest
     @DisplayName("A+B 테스트")
+    @ParameterizedTest
     @CsvSource(value = {"1 2:3", "5 5:10", "100 101:201", "1 7:8", "12 29:41"}, delimiter = ':')
     void test(String input, String expected) throws IOException {
         System.out.println("A+B : https://www.acmicpc.net/problem/1000");
 
         BufferedReader br_given = new BufferedReader(new StringReader(input));
+        br_given.close();
 
         assertEquals(expected, String.valueOf(No1000.solve(br_given)));
     }
--- src/test/java/boj/problems/step1/No1001Test.java
+++ src/test/java/boj/problems/step1/No1001Test.java
@@ -18,6 +18,7 @@ public class No1001Test {
         System.out.println("A-B : https://www.acmicpc.net/problem/1001");
 
         BufferedReader br_given = new BufferedReader(new StringReader(given));
+        br_given.close();
 
         assertEquals(expected, String.valueOf(No1001.solve(br_given)));
     }
