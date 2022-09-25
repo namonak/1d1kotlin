268190f41579c93495da0971919b25b37390ef2f
namonak <namonak@gmail.com>
2022-09-25 23:12:03 +0900

    refactor(test): test code refactoring
    Changed the following:
     - change BufferedReader/BufferedWriter to @ParameterizedTest
     - apply CsvSource to input/output values

--- src/test/java/boj/problems/step2/No1330Test.java
+++ src/test/java/boj/problems/step2/No1330Test.java
@@ -1,55 +1,26 @@
 package boj.problems.step2;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class No1330Test {
-    static class 두_수_비교하기_테스트케이스 {
-        String given;
-        String want;
-
-        public 두_수_비교하기_테스트케이스(String given, String want) {
-            this.given = given;
-            this.want = want;
-        }
-    }
-
-    @Test
     @DisplayName("두 수 비교하기 테스트")
-    void 두_수_비교하기_테스트() throws IOException {
+    @ParameterizedTest
+    @CsvSource(value = {"1 2:<", "10 2:>", "5 5:=="}, delimiter = ':')
+    void test(String input, String expected) throws IOException {
         System.out.println("두 수 비교하기 : https://www.acmicpc.net/problem/1330");
 
-        두_수_비교하기_테스트케이스[] testCase = {
-            new 두_수_비교하기_테스트케이스("1 2", "<"),
-            new 두_수_비교하기_테스트케이스("10 2", ">"),
-            new 두_수_비교하기_테스트케이스("5 5", "=="),
-        };
+        BufferedReader br_given = new BufferedReader(new StringReader(input));
 
-        for (int i = 0; i < testCase.length; i++) {
-            BufferedReader br_given = new BufferedReader(new StringReader(testCase[i].given));
-            BufferedReader br_want = new BufferedReader(new StringReader(testCase[i].want));
-            StringWriter sw = new StringWriter();
-            BufferedWriter bw_got = new BufferedWriter(sw);
+        assertEquals(expected, No1330.solve(br_given));
 
-            No1330.solve(br_given, bw_got);
-
-            bw_got.close();
-
-            BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-            String got = br_got.lines().collect(Collectors.joining());
-            String want = br_want.lines().collect(Collectors.joining());
-
-            assertEquals(want, got);
-        }
+        br_given.close();
     }
 }
