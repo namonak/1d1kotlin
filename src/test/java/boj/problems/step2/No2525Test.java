6be9b821c4b164c26920751fa6f2fa3ed133ef1e
namonak <namonak@gmail.com>
2022-09-27 23:26:10 +0900

    refactor(test): test code refactoring
    Changed the following:
     - change BufferedReader/BufferedWriter to @ParameterizedTest
     - apply @CsvSource to input/output values

--- src/test/java/boj/problems/step2/No2525Test.java
+++ src/test/java/boj/problems/step2/No2525Test.java
@@ -1,55 +1,26 @@
 package boj.problems.step2;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class No2525Test {
-    static class 오븐_시계_테스트케이스{
-        String given;
-        String want;
-
-        public 오븐_시계_테스트케이스(String given, String want) {
-            this.given = given;
-            this.want = want;
-        }
-    }
-
-    @Test
     @DisplayName("오븐 시계 테스트")
-    void 오븐_시계_테스트() throws IOException {
+    @ParameterizedTest
+    @CsvSource (value = {"'14 30\n20':14 50", "'17 40\n80':19 0", "'23 48\n25':0 13"}, delimiter = ':')
+    void 오븐_시계_테스트(String given, String expected) throws IOException {
         System.out.println("오븐 시계 : https://www.acmicpc.net/problem/2525");
 
-        오븐_시계_테스트케이스[] testCase = {
-                new 오븐_시계_테스트케이스("14 30" + System.lineSeparator() + "20", "14 50"),
-                new 오븐_시계_테스트케이스("17 40" + System.lineSeparator() + "80", "19 0"),
-                new 오븐_시계_테스트케이스("23 48" + System.lineSeparator() + "25", "0 13"),
-        };
+        BufferedReader br_given = new BufferedReader(new StringReader(given));
 
-        for (int i = 0; i < testCase.length; i++) {
-            BufferedReader br_given = new BufferedReader(new StringReader(testCase[i].given));
-            BufferedReader br_want = new BufferedReader(new StringReader(testCase[i].want));
-            StringWriter sw = new StringWriter();
-            BufferedWriter bw_got = new BufferedWriter(sw);
+        assertEquals(expected, No2525.solve(br_given));
 
-            No2525.solve(br_given, bw_got);
-
-            bw_got.close();
-
-            BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-            String got = br_got.lines().collect(Collectors.joining());
-            String want = br_want.lines().collect(Collectors.joining());
-
-            assertEquals(want, got);
-        }
+        br_given.close();
     }
 }
