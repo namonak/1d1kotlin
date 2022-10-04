126dce70ba2ea86299fa35f71f889444ee52faa6
namonak <namonak@gmail.com>
2022-10-04 23:04:52 +0900

    refactor: refactoring test code and production code of No11653

--- src/main/java/boj/problems/step8/No11653.java
+++ src/main/java/boj/problems/step8/No11653.java
@@ -11,22 +11,37 @@ public class No11653 {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        solve(input, output);
+        output.write(solve(input) + "\n");
 
+        input.close();
         output.flush();
         output.close();
     }
 
-    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+    static String solve(BufferedReader input) throws IOException {
         int number = Integer.parseInt(input.readLine());
         int sqrtNumber = (int)Math.sqrt(number);
 
+        return getResult(number, sqrtNumber);
+    }
+
+    private static String getResult(int number, int sqrtNumber) {
+        StringBuilder sb = new StringBuilder();
+
         for (int div = 2; div <= sqrtNumber; ++div) {
-            while (number % div == 0) {
-                number /= div;
-                output.write(div + "\n");
-            }
+            number = getNumber(number, sb, div);
         }
-        if (number > 1) output.write(number + "\n");
+        if (number > 1) {
+            sb.append(number).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static int getNumber(int number, StringBuilder sb, int div) {
+        while (number % div == 0) {
+            number /= div;
+            sb.append(div).append("\n");
+        }
+        return number;
     }
 }
