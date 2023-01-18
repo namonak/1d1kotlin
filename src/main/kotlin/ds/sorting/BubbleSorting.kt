15d1f3fc3a14f097353752ab10d3a8331540a3b2
namonak <namonak@gmail.com>
2023-01-18 21:45:38 +0900

    refactor(ds): update test code and production code of BubbleSorting

--- src/main/kotlin/ds/sorting/BubbleSorting.kt
+++ src/main/kotlin/ds/sorting/BubbleSorting.kt
@@ -2,7 +2,7 @@ package ds.sorting
 
 class BubbleSorting {
     // O(N^2), stable
-    fun sort(array: IntArray): IntArray {
+    fun sort(array: Array<Int>): Array<Int> {
         for (i in array.indices) {
             for (j in 1 until array.size - i) {
                 if (array[j - 1] > array[j]) {
--- src/test/kotlin/ds/sorting/TestBubleSorting.kt
+++ src/test/kotlin/ds/sorting/TestBubleSorting.kt
@@ -3,26 +3,16 @@ package ds.sorting
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-data class BubbleSortingTestCase(val given: IntArray, val want: IntArray)
-
 class TestBubbleSorting {
-    private val mTestBubbleSorting = arrayOf(
-        BubbleSortingTestCase(
-            intArrayOf(9, 3, 5, 7, 1),
-            intArrayOf(1, 3, 5, 7, 9)
-        )
-    )
-
     @Test
     fun testSort() {
-        val bubbleSorting = BubbleSorting()
+        // given
+        val given = arrayOf(9, 3, 5, 7, 1)
 
-        mTestBubbleSorting.forEach {
-            val result = bubbleSorting.sort(it.given)
+        // when
+        val actual = BubbleSorting().sort(given)
 
-            for (i in it.want.indices) {
-                assertThat(it.want[i]).isEqualTo(result[i])
-            }
-        }
+        // then
+        assertThat(actual).isEqualTo(arrayOf(1, 3, 5, 7, 9))
     }
 }
