b6e1b0f765f94ab89c5915627861c7917dcd3a75
namonak <namonak@gmail.com>
2023-01-21 21:21:12 +0900

    refactor(ds): update test code and production code of QuickSelect

--- src/test/kotlin/ds/sorting/TestQuickSelect.kt
+++ src/test/kotlin/ds/sorting/TestQuickSelect.kt
@@ -3,24 +3,16 @@ package ds.sorting
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-data class QuickSelectTestCase(val given: IntArray, val want: Int)
-
 class TestQuickSelect {
-    private val mQuickSelectTestCase = arrayOf(
-        QuickSelectTestCase(
-            intArrayOf(3, 5, 9, 1, 2, 4, 7),
-            5
-        )
-    )
-
     @Test
     fun testSort() {
-        val quickSelect = QuickSelect()
+        // given
+        val given = arrayOf(3, 5, 9, 1, 2, 4, 7)
 
-        mQuickSelectTestCase.forEach {
-            val result = quickSelect.select(it.given, 6)
+        // when
+        val actual = QuickSelect().select(given, 6)
 
-            assertThat(it.want).isEqualTo(result)
-        }
+        // then
+        assertThat(actual).isEqualTo(9)
     }
 }
