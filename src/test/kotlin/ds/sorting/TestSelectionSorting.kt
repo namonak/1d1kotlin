aac91ae75f4055de042fae2fb551f6b174fcd44c
namonak <namonak@gmail.com>
2023-01-26 23:27:51 +0900

    refactor(ds): update test code and production code of SelectionSorting

--- src/test/kotlin/ds/sorting/TestSelectionSorting.kt
+++ src/test/kotlin/ds/sorting/TestSelectionSorting.kt
@@ -3,26 +3,16 @@ package ds.sorting
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-data class SelectionSortingTestCase(val given: IntArray, val want: IntArray)
-
 class TestSelectionSorting {
-    private val mTestSelectionSortingTestCase = arrayOf(
-        SelectionSortingTestCase(
-            intArrayOf(9, 3, 5, 7, 1),
-            intArrayOf(1, 3, 5, 7, 9),
-        )
-    )
-
     @Test
     fun testSort() {
-        val selectionSorting = SelectionSorting()
+        // given
+        val given = arrayOf(9, 3, 5, 7, 1)
 
-        mTestSelectionSortingTestCase.forEach {
-            val result = selectionSorting.sort(it.given)
+        // when
+        val actual = SelectionSorting().sort(given)
 
-            for (i in it.want.indices) {
-                assertThat(it.want[i]).isEqualTo(result[i])
-            }
-        }
+        // then
+        assertThat(actual).isEqualTo(arrayOf(1, 3, 5, 7, 9))
     }
 }
