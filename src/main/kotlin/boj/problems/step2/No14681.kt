d1f5d25bc58db9c11f241e01b6589f85ad099c6f
namonak <namonak@gmail.com>
2023-02-16 15:43:33 +0900

    refactor: refactoring test code and production code of No14681

--- src/main/kotlin/boj/problems/step2/No14681.kt
+++ src/main/kotlin/boj/problems/step2/No14681.kt
@@ -1,19 +1,20 @@
 package boj.problems.step2
 
-import java.io.*
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 object No14681 {
-    @Throws(IOException::class)
     @JvmStatic
+    @Throws(IOException::class)
     fun main(args: Array<String>) {
         val input = BufferedReader(InputStreamReader(System.`in`))
         val output = BufferedWriter(OutputStreamWriter(System.out))
-        output.write(
-            """
-                ${solve(input)}
-                
-                """.trimIndent()
-        )
+
+        output.write(solve(input))
+
         input.close()
         output.flush()
         output.close()
@@ -27,14 +28,11 @@ object No14681 {
     }
 
     private fun getQuadrant(x: Int, y: Int): Int {
-        if (x > 0 && y > 0) {
-            return 1
+        return when {
+            x > 0 && y > 0 -> 1
+            x < 0 && y > 0 -> 2
+            x < 0 && y < 0 -> 3
+            else -> 4
         }
-        if (x < 0 && y > 0) {
-            return 2
-        }
-        return if (x < 0 && y < 0) {
-            3
-        } else 4
     }
 }
--- src/test/kotlin/boj/problems/step2/No14681Test.kt
+++ src/test/kotlin/boj/problems/step2/No14681Test.kt
@@ -1,6 +1,6 @@
 package boj.problems.step2
 
-import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -15,10 +15,18 @@ class No14681Test {
     @Throws(
         IOException::class
     )
-    fun test(given: String?, expected: String?) {
+    fun test(input: String, expected: String?) {
         println("사분면 고르기 : https://www.acmicpc.net/problem/14681")
-        val br_given = BufferedReader(StringReader(given))
-        Assertions.assertEquals(expected, java.lang.String.valueOf(No14681.solve(br_given)))
-        br_given.close()
+
+        // given
+        val given = BufferedReader(StringReader(input))
+
+        // when
+        val actual = No14681.solve(given).toString()
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+
+        given.close()
     }
 }
