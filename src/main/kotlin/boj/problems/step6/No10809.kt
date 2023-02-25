2a3f1f0cd7017b0c40d364f661b053e12cbb5832
namonak <namonak@gmail.com>
2023-02-25 21:02:40 +0900

    refactor: refactoring test code and production code of No10809

--- src/main/kotlin/boj/problems/step6/No10809.kt
+++ src/main/kotlin/boj/problems/step6/No10809.kt
@@ -2,70 +2,40 @@ package boj.problems.step6
 
 import java.io.BufferedReader
 import java.io.BufferedWriter
-import java.io.IOException
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 object No10809 {
     private const val ALPHABET_NUMBER = 26
 
-    @JvmStatic
-    @Throws(IOException::class)
-    fun main(args: Array<String>) {
-        val input = BufferedReader(InputStreamReader(System.`in`))
-        val output = BufferedWriter(OutputStreamWriter(System.out))
-
-        output.write(solve(input))
-
-        input.close()
-        output.flush()
-        output.close()
+    fun solve(input: String): String {
+        val alphabet = IntArray(ALPHABET_NUMBER) { -1 }
+        updateAlphabetArray(input, alphabet)
+        return alphabet.joinToString(separator = " ")
     }
 
-    fun solve(input: BufferedReader): String {
-        val word = checkInput(input)
-        val alphabet = initAlphabetArray()
-        updateAlphabetArray(word, alphabet)
-        return makeResult(alphabet)
-    }
-
-    private fun makeResult(alphabet: IntArray): String {
-        val sb = StringBuilder()
-        for (i in alphabet.indices) {
-            sb.append(alphabet[i]).append(" ")
-        }
-        sb.setLength(sb.length - 1)
-        return sb.toString()
-    }
-
-    private fun updateAlphabetArray(word: String?, alphabet: IntArray) {
-        for (i in 0 until word!!.length) {
-            checkWordIndex(word, alphabet, i)
+    private fun updateAlphabetArray(input: String, alphabet: IntArray) {
+        for (i in input.indices) {
+            checkWordIndex(input, alphabet, i)
         }
     }
 
-    private fun checkWordIndex(word: String?, alphabet: IntArray, i: Int) {
-        val index = word!![i].code - 'a'.code
+    private fun checkWordIndex(input: String, alphabet: IntArray, i: Int) {
+        val index = input[i].code - 'a'.code
         if (alphabet[index] == -1) {
             alphabet[index] = i
         }
     }
 
-    private fun initAlphabetArray(): IntArray {
-        val alphabet = IntArray(ALPHABET_NUMBER)
-        for (i in alphabet.indices) {
-            alphabet[i] = -1
-        }
-        return alphabet
-    }
+}
 
-    private fun checkInput(input: BufferedReader): String? {
-        var word: String? = null
-        try {
-            word = input.readLine()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return word
-    }
+fun main() {
+    val input = BufferedReader(InputStreamReader(System.`in`))
+    val output = BufferedWriter(OutputStreamWriter(System.out))
+
+    output.write("${No10809.solve(input.readLine())}\n")
+
+    input.close()
+    output.flush()
+    output.close()
 }
--- src/test/kotlin/boj/problems/step6/No10809Test.kt
+++ src/test/kotlin/boj/problems/step6/No10809Test.kt
@@ -1,27 +1,19 @@
 package boj.problems.step6
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.StringReader
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-class No10809Test {
-    @DisplayName("알파벳 찾기")
-    @ParameterizedTest
-    @CsvSource(
-        value = ["baekjoon:1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1"],
-        delimiter = ':'
-    )
-    @Throws(
-        IOException::class
-    )
-    fun test(given: String?, expected: String?) {
-        println("알파벳 찾기 : https://www.acmicpc.net/problem/10809")
-        val br_given = BufferedReader(StringReader(given))
-        assertThat(No10809.solve(br_given)).isEqualTo(expected)
-        br_given.close()
+class No10809Test : StringSpec() {
+    init {
+        "알파벳 찾기" {
+            val given = "baekjoon"
+            val expected = "1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1"
+
+            // Run the test
+            val actual = No10809.solve(given)
+
+            // Verify the result
+            actual shouldBe expected
+        }
     }
 }
