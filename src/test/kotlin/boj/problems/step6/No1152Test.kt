71981862ac3463ad4542bcd3b900b8310f9fa86c
namonak <namonak@gmail.com>
2023-02-27 20:52:33 +0900

    refactor: update test code of No1152

--- src/test/kotlin/boj/problems/step6/No1152Test.kt
+++ src/test/kotlin/boj/problems/step6/No1152Test.kt
@@ -1,33 +1,28 @@
 package boj.problems.step6
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.StringReader
 
-internal class No1152Test {
-    @DisplayName("단어의 개수 테스트")
-    @ParameterizedTest
-    @CsvSource(
-        value = ["The Curious Case of Benjamin Button:6", "The first character is a blank:6", "The last character is a blank:6", "Mazatneunde Wae Teullyeoyo:3", "Teullinika Teullyeotzi:2"],
-        delimiter = ':'
-    )
-    @Throws(
-        IOException::class
-    )
-    fun test(given: String, expected: String?) {
-        println("단어의 개수 : https://www.acmicpc.net/problem/1152")
+internal class No1152Test : StringSpec({
+    "단어의 개수 테스트" {
+        val (input, expected) = row(
+            "The Curious Case of Benjamin Button", "6",
+            "The first character is a blank", "6",
+            "The last character is a blank", "6",
+            "Mazatneunde Wae Teullyeoyo", "3",
+            "Teullinika Teullyeotzi", "2"
+        )
 
         // given
-        val br_given = BufferedReader(StringReader(given))
+        val given = BufferedReader(StringReader(input))
 
         // when
-        val actual: String = java.lang.String.valueOf(No1152.solve(br_given))
+        val actual: String = No1152.solve(given).toString()
 
         // then
-        Assertions.assertEquals(expected, actual)
+        actual shouldBe expected
     }
-}
+})
