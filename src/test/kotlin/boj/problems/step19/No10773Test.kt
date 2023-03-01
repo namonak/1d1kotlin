3cf757fdde0fcbbaea7483a44129096e833e93f1
namonak <namonak@gmail.com>
2023-03-01 22:04:29 +0900

    refactor: update test code of No10773

--- src/test/kotlin/boj/problems/step19/No10773Test.kt
+++ src/test/kotlin/boj/problems/step19/No10773Test.kt
@@ -1,24 +1,34 @@
 package boj.problems.step19
 
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.StringReader
 
-class No10773Test {
-    @DisplayName("제로")
-    @ParameterizedTest
-    @CsvSource(value = ["'4\n3\n0\n4\n0':0", "'10\n1\n3\n5\n4\n0\n0\n7\n0\n0\n6':7", "'0\n0\n0\n':0"], delimiter = ':')
-    @Throws(
-        IOException::class
-    )
-    fun test(given: String, expected: String) {
-        println("제로 : https://www.acmicpc.net/problem/10773")
-        val br_given = BufferedReader(StringReader(given))
-        Assertions.assertThat(java.lang.String.valueOf(No10773.solve(br_given))).isEqualTo(expected)
-        br_given.close()
+class No10773Test : StringSpec() {
+    init {
+        "제로" {
+            // Define the test cases as a Table
+            val testCases = listOf(
+                TestCaseData("4\n3\n0\n4\n0", "0"),
+                TestCaseData("10\n1\n3\n5\n4\n0\n0\n7\n0\n0\n6", "7"),
+                TestCaseData("0\n0\n0\n", "0")
+            )
+
+            // Run each test case
+            testCases.forEach {
+                val input = it.input
+                val expected = it.expected
+
+                // Run the test
+                val actual = No10773.solve(BufferedReader(StringReader(input))).toString()
+
+                // Verify the result
+                actual shouldBe expected
+            }
+        }
     }
+
+    // Define a data class to represent each test case
+    data class TestCaseData(val input: String, val expected: String)
 }
