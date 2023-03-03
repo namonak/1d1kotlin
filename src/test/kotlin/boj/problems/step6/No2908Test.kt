4f0a5f21051f28a6fbc2621bd8badf212a90b0ed
namonak <namonak@gmail.com>
2023-03-03 15:20:31 +0900

    refactor: update test code of No2908

--- src/test/kotlin/boj/problems/step6/No2908Test.kt
+++ src/test/kotlin/boj/problems/step6/No2908Test.kt
@@ -1,24 +1,31 @@
 package boj.problems.step6
 
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.StringReader
 
-class No2908Test {
-    @DisplayName("상수 테스트")
-    @ParameterizedTest
-    @CsvSource(value = ["734 893:437", "221 231:132", "839 237:938"], delimiter = ':')
-    @Throws(
-        IOException::class
-    )
-    fun test(given: String?, expected: String?) {
-        println("상수 : https://www.acmicpc.net/problem/2908")
-        val br_given = BufferedReader(StringReader(given))
-        Assertions.assertThat(java.lang.String.valueOf(No2908.solve(br_given))).isEqualTo(expected)
-        br_given.close()
+class No2908Test : StringSpec() {
+    init {
+        "상수 : https://www.acmicpc.net/problem/2908" {
+            // Define the test cases as a Table
+            val testCases = listOf(
+                TestCaseData("734 893", 437),
+                TestCaseData("221 231", 132),
+                TestCaseData("839 237", 938)
+            )
+
+            testCases.forEach { testCase ->
+                val given = testCase.given
+                val expected = testCase.expected
+
+                val actual = No2908.solve(BufferedReader(StringReader(given)))
+
+                actual shouldBe expected
+            }
+        }
     }
+
+    // Define a data class to represent each test case
+    data class TestCaseData(val given: String, val expected: Int)
 }
