0c5086fd7be51f98902674b620c29a8796284703
namonak <namonak@gmail.com>
2023-03-09 17:25:46 +0900

    refactor: update test code of No14681

--- src/test/kotlin/boj/problems/step2/No14681Test.kt
+++ src/test/kotlin/boj/problems/step2/No14681Test.kt
@@ -1,34 +1,27 @@
 package boj.problems.step2
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
-class No14681Test : StringSpec() {
-    init {
-        "사분면 고르기 테스트" {
-            // Define the test cases as a Table
-            val testCases = listOf(
-                TestCaseData("12\n5\n", "1"),
-                TestCaseData("9\n-13\n", "4")
-            )
+class No14681Test : StringSpec({
+    "사분면 고르기 테스트" {
+        // Define the test cases as a Table
+        val testCases = listOf(
+            row("12\n5\n", "1"),
+            row("9\n-13\n", "4")
+        )
 
-            // Run each test case
-            testCases.forEach {
-                val input = it.input
-                val expected = it.expected
+        // Run each test case
+        testCases.forEach { (input, expected) ->
+            // Create a new instance of the class under test for each test case
+            val no14681 = No14681(InputProcessor14681(), OutputProcessor14681())
 
-                // Create a new instance of the class under test for each test case
-                val no14681 = No14681(InputProcessor14681(), OutputProcessor14681())
+            // Run the test
+            val actual = no14681.solve(input)
 
-                // Run the test
-                val actual = no14681.solve(input)
-
-                // Verify the result
-                actual shouldBe expected
-            }
+            // Verify the result
+            actual shouldBe expected
         }
     }
-
-    // Define a data class to represent each test case
-    data class TestCaseData(val input: String, val expected: String)
-}
+})
