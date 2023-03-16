2e828cb25232787c6b2178f90f80aea0f2aa1a25
namonak <namonak@gmail.com>
2023-03-16 12:06:10 +0900

    refactor: update test code of No11654

--- src/test/kotlin/boj/problems/step6/No11654Test.kt
+++ src/test/kotlin/boj/problems/step6/No11654Test.kt
@@ -1,38 +1,31 @@
 package boj.problems.step6
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
-class No11654Test : StringSpec() {
-    init {
-        "아스키 코드 테스트" {
-            // Define the test cases as a Table
-            val testCases = listOf(
-                TestCaseData("A", "65"),
-                TestCaseData("C", "67"),
-                TestCaseData("0", "48"),
-                TestCaseData("9", "57"),
-                TestCaseData("a", "97"),
-                TestCaseData("z", "122")
-            )
+class No11654Test : StringSpec({
+    "아스키 코드 테스트" {
+        // Define the test cases as a Table
+        val testCases = listOf(
+            row("A", "65"),
+            row("C", "67"),
+            row("0", "48"),
+            row("9", "57"),
+            row("a", "97"),
+            row("z", "122")
+        )
 
-            // Run each test case
-            testCases.forEach {
-                val given = it.given
-                val expected = it.expected
+        // Run each test case
+        testCases.forEach { (given, expected) ->
+            // Create a new instance of the class under test for each test case
+            val no11654 = No11654(InputProcessor(), OutputProcessor())
 
-                // Create a new instance of the class under test for each test case
-                val no11654 = No11654(InputProcessor(), OutputProcessor())
+            // Run the test
+            val actual = no11654.solve(given)
 
-                // Run the test
-                val actual = no11654.solve(given)
-
-                // Verify the result
-                actual shouldBe expected
-            }
+            // Verify the result
+            actual shouldBe expected
         }
     }
-
-    // Define a data class to represent each test case
-    data class TestCaseData(val given: String, val expected: String)
-}
+})
