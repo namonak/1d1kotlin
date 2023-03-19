749ffc2e4d30a481bdeaf8a5b08d591f48da2ab8
namonak <namonak@gmail.com>
2023-03-19 20:45:03 +0900

    refactor: update test code of No2480

--- src/test/kotlin/boj/problems/step2/No2480Test.kt
+++ src/test/kotlin/boj/problems/step2/No2480Test.kt
@@ -1,31 +1,31 @@
 package boj.problems.step2
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.StringReader
 
-class No2480Test {
-    @DisplayName("주사위 세개 테스트")
-    @ParameterizedTest
-    @CsvSource(value = ["3 3 6:1300", "2 2 2:12000", "6 2 5:600"], delimiter = ':')
-    @Throws(
-        IOException::class
-    )
-    fun test(given: String?, expected: String?) {
+class No2480Test : StringSpec({
+    "주사위 세개 테스트" {
         println("주사위 세게 : https://www.acmicpc.net/problem/2480")
 
-        // given
-        val br_given = BufferedReader(StringReader(given))
+        // Define the test cases as a Table
+        val testCases = listOf(
+            row("3 3 6", 1300),
+            row("2 2 2", 12000),
+            row("6 2 5", 600),
+        )
 
-        // when
-        val actual = No2480.solve(br_given)
+        // Run each test case
+        testCases.forEach { (given, expected) ->
+            // Run the test
+            val input = BufferedReader(StringReader(given))
 
-        // then
-        assertEquals(expected, java.lang.String.valueOf(actual))
-        br_given.close()
+            val actual = No2480.solve(input)
+
+            // verify the result
+            actual shouldBe expected
+        }
     }
-}
+})
