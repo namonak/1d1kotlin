322cc0b8b01106af647182bbb8d12a2dbfe2ed69
namonak <namonak@gmail.com>
2023-03-20 17:25:18 +0900

    refactor: update test code of No10828

--- src/test/kotlin/boj/problems/step19/No10828Test.kt
+++ src/test/kotlin/boj/problems/step19/No10828Test.kt
@@ -1,26 +1,23 @@
 package boj.problems.step19
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import java.io.File
 
 class No10828Test : StringSpec({
-    val path = File(".")
-    val input = arrayOf(
-        "No10828_input_1.txt",
-        "No10828_input_2.txt"
-    )
-
-    val output = arrayOf(
-        "No10828_output_1.txt",
-        "No10828_output_2.txt"
-    )
-
     "스택" {
-        input.indices.forEach { i ->
+        val path = File(".")
+
+        val testCases = listOf(
+            row("No10828_input_1.txt", "No10828_output_1.txt"),
+            row("No10828_input_2.txt", "No10828_output_2.txt")
+        )
+
+        testCases.forEach { (input, output) ->
             // given
-            val given = File(path, "src/test/kotlin/boj/problems/step19/${input[i]}").bufferedReader()
-            val expected = File(path, "src/test/kotlin/boj/problems/step19/${output[i]}").readText()
+            val given = File(path, "src/test/kotlin/boj/problems/step19/$input").bufferedReader()
+            val expected = File(path, "src/test/kotlin/boj/problems/step19/$output").readText()
 
             // when
             val actual = No10828.solve(given)
