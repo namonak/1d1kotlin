d79317de3a0ecdc9ed6ac69072c721310a63f46c
namonak <namonak@gmail.com>
2023-03-23 18:13:59 +0900

    refactor: update test code of No10809

--- src/test/kotlin/boj/problems/step6/No10809Test.kt
+++ src/test/kotlin/boj/problems/step6/No10809Test.kt
@@ -3,17 +3,15 @@ package boj.problems.step6
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class No10809Test : StringSpec() {
-    init {
-        "알파벳 찾기" {
-            val given = "baekjoon"
-            val expected = "1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1"
+class No10809Test : StringSpec({
+    "알파벳 찾기" {
+        val given = "baekjoon"
+        val expected = "1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1"
 
-            // Run the test
-            val actual = No10809.solve(given)
+        // Run the test
+        val actual = No10809.solve(given)
 
-            // Verify the result
-            actual shouldBe expected
-        }
+        // Verify the result
+        actual shouldBe expected
     }
-}
+})
