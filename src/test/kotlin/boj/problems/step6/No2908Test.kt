8456947a0a4fde9172513c01c45902894b7ff055
namonak <namonak@gmail.com>
2023-03-24 17:08:48 +0900

    refactor: update test code of No2908

--- src/test/kotlin/boj/problems/step6/No2908Test.kt
+++ src/test/kotlin/boj/problems/step6/No2908Test.kt
@@ -1,31 +1,24 @@
 package boj.problems.step6
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import java.io.BufferedReader
 import java.io.StringReader
 
-class No2908Test : StringSpec() {
-    init {
-        "상수 : https://www.acmicpc.net/problem/2908" {
-            // Define the test cases as a Table
-            val testCases = listOf(
-                TestCaseData("734 893", 437),
-                TestCaseData("221 231", 132),
-                TestCaseData("839 237", 938)
-            )
+class No2908Test : StringSpec({
+    "상수 : https://www.acmicpc.net/problem/2908" {
+        // Define the test cases as a Table
+        val testCases = listOf(
+            row("734 893", 437),
+            row("221 231", 132),
+            row("839 237", 938)
+        )
 
-            testCases.forEach { testCase ->
-                val given = testCase.given
-                val expected = testCase.expected
+        testCases.forEach { (given, expected) ->
+            val actual = No2908.solve(BufferedReader(StringReader(given)))
 
-                val actual = No2908.solve(BufferedReader(StringReader(given)))
-
-                actual shouldBe expected
-            }
+            actual shouldBe expected
         }
     }
-
-    // Define a data class to represent each test case
-    data class TestCaseData(val given: String, val expected: Int)
-}
+})
