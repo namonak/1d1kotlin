c7e9ddc8bc67d3db3e47ef4b85f233dbcef7c11f
namonak <namonak@gmail.com>
2023-03-25 18:27:30 +0900

    refactor: update test code of No2525

--- src/test/kotlin/boj/problems/step2/No2525Test.kt
+++ src/test/kotlin/boj/problems/step2/No2525Test.kt
@@ -1,33 +1,29 @@
 package boj.problems.step2
 
 import boj.problems.step2.No2525.solve
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.StringReader
 
-class No2525Test {
-    @DisplayName("오븐 시계 테스트")
-    @ParameterizedTest
-    @CsvSource(value = ["'14 30\n20':14 50", "'17 40\n80':19 0", "'23 48\n25':0 13"], delimiter = ':')
-    @Throws(
-        IOException::class
-    )
-    fun test(input: String, expected: String?) {
-        println("오븐 시계 : https://www.acmicpc.net/problem/2525")
+class No2525Test : StringSpec({
+    "오븐 시계 테스트" {
+        val testCases = listOf(
+            row("14 30\n20", "14 50"),
+            row("17 40\n80", "19 0"),
+            row("23 48\n25", "0 13")
+        )
 
-        // given
-        val given = BufferedReader(StringReader(input))
+        testCases.forEach { (input, expected) ->
+            // given
+            val given = BufferedReader(StringReader(input))
 
-        // when
-        val actual = solve(given)
+            // when
+            val actual = solve(given)
 
-        // then
-        assertThat(actual).isEqualTo(expected)
-
-        given.close()
+            // then
+            assertThat(actual).isEqualTo(expected)
+        }
     }
-}
+})
