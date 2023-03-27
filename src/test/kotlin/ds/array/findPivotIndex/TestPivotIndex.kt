578d27c13830bb05a658063f9771bf4130f217ed
namonak <namonak@gmail.com>
2023-03-27 22:20:38 +0900

    refactor: update test code of PivotIndex

--- src/test/kotlin/ds/array/findPivotIndex/TestPivotIndex.kt
+++ src/test/kotlin/ds/array/findPivotIndex/TestPivotIndex.kt
@@ -1,29 +1,23 @@
 package ds.array.findPivotIndex
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-data class TestCase(val given: IntArray, val want: Int)
-
-class TestPivotIndex {
-    @Test
-    fun testPivotIndex() {
+class TestPivotIndex : StringSpec({
+    "testPivotIndex" {
+        // define test cases
         val testCase = arrayOf(
-            TestCase(
-                intArrayOf(1, 8, 2, 9, 2, 3, 6),
-                3
-            ),
-            TestCase(
-                intArrayOf(2, 5, 7),
-                -1
-            )
+            row(intArrayOf(1, 8, 2, 9, 2, 3, 6), 3),
+            row(intArrayOf(2, 5, 7), -1)
         )
-        testCase.forEach {
+
+        testCase.forEach { (given, expected) ->
             val pivotIndex = PivotIndex()
 
-            val got = pivotIndex.pivotIndex(it.given)
+            val actual = pivotIndex.pivotIndex(given)
 
-            assertThat(it.want).isEqualTo(got)
+            actual shouldBe expected
         }
     }
-}
+})
