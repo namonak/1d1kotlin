25d49d507550d5e9bd7771ea72e24bd35a46d26c
namonak <namonak@gmail.com>
2023-03-28 23:29:23 +0900

    refactor: update test code of MoveZeros

--- src/test/kotlin/ds/array/moveZeros/TestMoveZeros.kt
+++ src/test/kotlin/ds/array/moveZeros/TestMoveZeros.kt
@@ -1,31 +1,22 @@
 package ds.array.moveZeros
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-data class TestCase(val given: IntArray, val want: IntArray)
-
-class TestMoveZeros {
-    @Test
-    fun testBinarySearch() {
+class TestMoveZeros : StringSpec({
+    "testMoveZeros" {
         val testCase = arrayOf(
-            TestCase(
-                intArrayOf(0, 5, 0, 7, 6, 3),
-                intArrayOf(5, 7, 6, 3, 0, 0)
-            ),
-            TestCase(
-                intArrayOf(0, 0, 0, 0, 5, 9, 3),
-                intArrayOf(5, 9, 3, 0, 0, 0, 0)
-            ),
+            row(intArrayOf(0, 5, 0, 7, 6, 3), intArrayOf(5, 7, 6, 3, 0, 0)),
+            row(intArrayOf(0, 0, 0, 0, 5, 9, 3), intArrayOf(5, 9, 3, 0, 0, 0, 0)),
         )
 
-        val moveZeros = MoveZeros()
+        testCase.forEach { (given, expected) ->
+            val moveZeros = MoveZeros()
 
-        testCase.forEach {
-            var result = moveZeros.MoveZeros(it.given)
-            for (i in result.indices) {
-                assertThat(it.want[i]).isEqualTo(result[i])
-            }
+            val actual = moveZeros.MoveZeros(given)
+
+            actual shouldBe expected
         }
     }
-}
+})
