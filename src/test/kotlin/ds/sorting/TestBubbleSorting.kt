891fcc9cfd263c420b69e814403f22fbb1528800
namonak <namonak@gmail.com>
2023-03-30 18:15:43 +0900

    refactor(ds): update test code of BubbleSorting

--- src/test/kotlin/ds/sorting/TestBubbleSorting.kt
+++ src/test/kotlin/ds/sorting/TestBubbleSorting.kt
@@ -1,18 +1,18 @@
 package ds.sorting
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-class TestBubbleSorting {
-    @Test
-    fun testSort() {
+class TestBubbleSorting : StringSpec({
+    "testBubbleSorting" {
         // given
         val given = arrayOf(9, 3, 5, 7, 1)
+        val expected = arrayOf(1, 3, 5, 7, 9)
 
         // when
         val actual = BubbleSorting().sort(given)
 
         // then
-        assertThat(actual).isEqualTo(arrayOf(1, 3, 5, 7, 9))
+        actual shouldBe expected
     }
-}
+})
