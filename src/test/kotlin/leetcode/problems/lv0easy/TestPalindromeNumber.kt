30f578733616288b695d33e18cfb58722ccfd383
namonak <namonak@gmail.com>
2023-04-02 23:03:26 +0900

    refactor(leetcode): update test code of PalindromeNumber

--- src/test/kotlin/leetcode/problems/lv0easy/TestPalindromeNumber.kt
+++ src/test/kotlin/leetcode/problems/lv0easy/TestPalindromeNumber.kt
@@ -1,22 +1,22 @@
 package leetcode.problems.lv0easy
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-class TestPalindromeNumber {
-    @ParameterizedTest
-    @CsvSource(
-        "121, true",
-        "1221, true",
-        "123, false",
-        "1234, false"
-    )
-    fun testPalindromeNumber(given: Int, want: Boolean) {
-        // when
-        val actual = PalindromeNumber().isPalindrome(given)
+class TestPalindromeNumber : StringSpec({
+    "testPalindromeNumber" {
+        val testCases = listOf(
+            row(121, true),
+            row(1221, true),
+            row(123, false),
+            row(1234, false),
+        )
 
-        // then
-        assertThat(actual).isEqualTo(want)
+        testCases.forEach { (given, expected) ->
+            val actual = PalindromeNumber().isPalindrome(given)
+
+            actual shouldBe expected
+        }
     }
-}
+})
