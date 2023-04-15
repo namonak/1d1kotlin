f7f99dafdc8cebbcc8248322b4c3fbd09375e91b
namonak <namonak@gmail.com>
2023-04-15 09:15:04 +0900

    refactor(leetcode): update test code of TwoSum

--- src/test/kotlin/leetcode/problems/lv0easy/TestTwoSum.kt
+++ src/test/kotlin/leetcode/problems/lv0easy/TestTwoSum.kt
@@ -1,52 +1,26 @@
 package leetcode.problems.lv0easy
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-data class TestData(val given1: IntArray, val given2: Int, val want: IntArray)
+class TestTwoSum : StringSpec({
+    "testTwoSum" {
+        val testCases = arrayOf(
+            row(intArrayOf(2, 7, 11, 15), 9, intArrayOf(0, 1)),
+            row(intArrayOf(3, 2, 4), 6, intArrayOf(1, 2)),
+            row(intArrayOf(3, 3), 6, intArrayOf(0, 1)),
+            row(intArrayOf(0, 4, 3, 0), 0, intArrayOf(0, 3)),
+            row(intArrayOf(-3, 4, 3, 90), 0, intArrayOf(0, 2)),
+            row(intArrayOf(-1, -2, -3, -4, -5), -8, intArrayOf(2, 4)),
+        )
 
-class TestTwoSum {
-    private val mTestData = arrayOf(
-        TestData(
-            given1 = intArrayOf(2, 7, 11, 15),
-            given2 = 9,
-            want = intArrayOf(0, 1)
-        ),
-        TestData(
-            given1 = intArrayOf(3, 2, 4),
-            given2 = 6,
-            want = intArrayOf(1, 2)
-        ),
-        TestData(
-            given1 = intArrayOf(3, 3),
-            given2 = 6,
-            want = intArrayOf(0, 1)
-        ),
-        TestData(
-            given1 = intArrayOf(0, 4, 3, 0),
-            given2 = 0,
-            want = intArrayOf(0, 3)
-        ),
-        TestData(
-            given1 = intArrayOf(-3, 4, 3, 90),
-            given2 = 0,
-            want = intArrayOf(0, 2)
-        ),
-
-        TestData(
-            given1 = intArrayOf(-1, -2, -3, -4, -5),
-            given2 = -8,
-            want = intArrayOf(2, 4)
-        ),
-    )
-
-    @Test
-    fun testTwoSum() {
         val twoSum = TwoSum()
 
-        for (testData in mTestData) {
-            val got = twoSum.twoSum(testData.given1, testData.given2)
-            assertThat(got).isEqualTo(testData.want)
+        testCases.forEach { (given0, given1, want) ->
+            val actual = twoSum.twoSum(given0, given1)
+
+            actual shouldBe want
         }
     }
-}
+})
