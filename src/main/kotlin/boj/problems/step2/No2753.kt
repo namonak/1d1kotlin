cadd67866a53cc0fd83007a64e4a30fc1b06513f
namonak <namonak@gmail.com>
2023-04-26 22:48:31 +0900

    refactor(boj): update production code of No2753

--- src/main/kotlin/boj/problems/step2/No2753.kt
+++ src/main/kotlin/boj/problems/step2/No2753.kt
@@ -8,23 +8,21 @@ import java.io.OutputStreamWriter
 
 object No2753 {
     @Throws(IOException::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val input = BufferedReader(InputStreamReader(System.`in`))
-        val output = BufferedWriter(OutputStreamWriter(System.out))
-
-        output.write("${solve(input)}\n")
-
-        input.close()
-        output.flush()
-        output.close()
-    }
-
-    @Throws(IOException::class)
     fun solve(input: BufferedReader): Int {
         val year = input.readLine().toInt()
         return if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             1
         } else 0
     }
+}
+
+fun main(args: Array<String>) {
+    val input = BufferedReader(InputStreamReader(System.`in`))
+    val output = BufferedWriter(OutputStreamWriter(System.out))
+
+    output.write("${No2753.solve(input)}\n")
+
+    input.close()
+    output.flush()
+    output.close()
 }
