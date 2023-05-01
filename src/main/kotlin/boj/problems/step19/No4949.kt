244029f761f557db30232fc34b0c11943ec6466c
namonak <namonak@gmail.com>
2023-05-02 00:44:41 +0900

    refactor(boj): update test code and production code of No4949

--- src/main/kotlin/boj/problems/step19/No4949.kt
+++ src/main/kotlin/boj/problems/step19/No4949.kt
@@ -1,12 +1,13 @@
 package boj.problems.step19
 
+import java.io.BufferedReader
 import java.util.Stack
 
 fun main() {
     val input = System.`in`.bufferedReader()
     val output = System.out.bufferedWriter()
 
-    output.write(No4949.solve(input.readLine()))
+    output.write(No4949.solve(input))
 
     input.close()
     output.flush()
@@ -14,7 +15,21 @@ fun main() {
 }
 
 object No4949 {
-    fun solve(input: String): String {
+    fun solve(input: BufferedReader): String {
+        val result = StringBuilder()
+
+        while (true) {
+            val line = input.readLine()
+            if (line == ".") {
+                break
+            }
+            result.append(checkParenthesis(line)).append("\n")
+        }
+
+        return result.toString().trim()
+    }
+
+    private fun checkParenthesis(input: String): String {
         val stack = Stack<Char>()
 
         input.forEach { c ->
--- src/test/kotlin/boj/problems/step19/No4949Test.kt
+++ src/test/kotlin/boj/problems/step19/No4949Test.kt
@@ -1,31 +1,21 @@
 package boj.problems.step19
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import java.io.File
 
 class No4949Test : StringSpec({
     "균형잡힌 세상 테스트" {
         println("균형잡힌 세상 : https://www.acmicpc.net/problem/4949")
 
-        // Define the test cases as a Table
-        val testCases = listOf(
-            row("So when I die (the [first] I will see in (heaven) is a score list).", "yes"),
-            row("[ first in ] ( first out ).", "yes"),
-            row("Half Moon tonight (At least it is better than no Moon at all].", "no"),
-            row("A rope may form )( a trail in a maze.", "no"),
-            row("Help( I[m being held prisoner in a fortune cookie factory)].", "no"),
-            row("([ (([( [ ] ) ( ) (( ))] )) ]).", "yes"),
-            row(" .", "yes"),
-            row(". .", "yes"),
-        )
+        // given
+        val given = File("src/test/kotlin/boj/problems/step19/No4949_input.txt").bufferedReader()
+        val expected = File("src/test/kotlin/boj/problems/step19/No4949_output.txt").readText()
 
-        testCases.forEach { (input, expected) ->
-            // Run the test
-            val actual = No4949.solve(input)
+        // when
+        val actual = No4949.solve(given)
 
-            // Verify the result
-            actual shouldBe expected
-        }
+        // then
+        actual shouldBe expected
     }
 })
