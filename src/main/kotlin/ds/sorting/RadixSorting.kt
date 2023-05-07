a7c1f2e6382b794ce0d531a1cde584b771524aae
namonak <namonak@gmail.com>
2023-05-07 14:42:04 +0900

    refactor(ds): update producton code of RadixSorting

--- src/main/kotlin/ds/sorting/RadixSorting.kt
+++ src/main/kotlin/ds/sorting/RadixSorting.kt
@@ -2,24 +2,34 @@ package ds.sorting
 
 class RadixSorting {
     fun sort(array: Array<Int>): Array<Int> {
-        val n = array.size
-        val m = array.maxOrNull() ?: 0
-        val exp = 1
-        val radix = 10
-        val bucket = Array(radix) { IntArray(n) }
-        for (i in 0 until n) {
-            val digit = (array[i] / exp) % radix
-            bucket[digit][i] = array[i]
-        }
-        var i = 0
-        for (j in 0 until radix) {
-            for (k in 0 until n) {
-                if (bucket[j][k] != 0) {
-                    array[i] = bucket[j][k]
-                    i++
+        val radix = 10 // 10진수 기준
+        var maxLength = false
+        var placement = 1
+
+        while (!maxLength) {
+            maxLength = true
+            val buckets = Array(radix) { mutableListOf<Int>() }
+
+            // 현재 자리수를 기준으로 버킷에 추가
+            for (i in array) {
+                val tmp = i / placement
+                buckets[tmp % radix].add(i)
+                if (maxLength && tmp > 0) {
+                    maxLength = false
                 }
             }
+
+            // 버킷에 있는 요소들을 다시 배열에 추가
+            var j = 0
+            for (b in 0 until radix) {
+                for (i in buckets[b]) {
+                    array[j++] = i
+                }
+                buckets[b].clear()
+            }
+            placement *= radix
         }
+
         return array
     }
 }
