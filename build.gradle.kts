24deab811cd012ddcc3458ae8cc92bab464c62e2
namonak <namonak@gmail.com>
2023-05-12 13:04:32 +0900

    build: upgrade to Kotlin 1.8.21 and Gradle 8.1.1 and Kotest 5.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.8.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.9.3")
     testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation("io.kotest", "kotest-runner-junit5", "5.6.1")
 }
 
 tasks {
