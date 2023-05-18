795c409bce298e8dec68f651a6373ec1f846b07c
namonak <namonak@gmail.com>
2023-05-18 10:55:12 +0900

    refactor(boj): update test code of No2292

--- src/test/kotlin/boj/problems/step7/No2292Test.kt
+++ src/test/kotlin/boj/problems/step7/No2292Test.kt
@@ -1,29 +1,24 @@
 package boj.problems.step7
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
-class No2292Test : StringSpec() {
-    init {
-        "벌집 테스트" {
-            println("벌집 : https://www.acmicpc.net/problem/2292")
+class No2292Test : StringSpec({
+    "벌집 : https://www.acmicpc.net/problem/2292" {
+        val testCases = listOf(
+            13 to 3,
+            1 to 1,
+            2 to 2,
+            7 to 2,
+            5 to 2,
+            37 to 4,
+            38 to 5,
+            64 to 6
+        )
 
-            val testCases = listOf(
-                row(13, 3),
-                row(1, 1),
-                row(2, 2),
-                row(7, 2),
-                row(5, 2),
-                row(37, 4),
-                row(38, 5),
-                row(64, 6)
-            )
-
-            testCases.forEach { (input, expected) ->
-                val actual = No2292.solve(input)
-                actual shouldBe expected
-            }
+        testCases.forEach { (input, expected) ->
+            val actual = No2292.solve(input)
+            actual shouldBe expected
         }
     }
-}
+})
