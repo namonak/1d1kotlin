f856543cd889dbb83c67a25b170e3548ebadcc08
namonak <namonak@gmail.com>
2023-05-24 10:43:00 +0900

    feat(boj): add test code and production code of No1735

---
+++ src/main/kotlin/boj/problems/step15/No1735.kt
@@ -0,0 +1,30 @@
+package boj.problems.step15
+
+import java.io.BufferedReader
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No1735.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No1735 {
+    fun solve(input: BufferedReader): String {
+        val (a1, b1) = input.readLine().split(" ").map { it.toInt() }
+        val (a2, b2) = input.readLine().split(" ").map { it.toInt() }
+
+        val a = a1 * b2 + a2 * b1
+        val b = b1 * b2
+
+        return "${a / gcd(a, b)} ${b / gcd(a, b)}"
+    }
+
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+}
---
+++ src/test/kotlin/boj/problems/step15/No1735Test.kt
@@ -0,0 +1,20 @@
+package boj.problems.step15
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+import java.io.StringReader
+
+class No1735Test : StringSpec({
+    "분수 합 : https://www.acmicpc.net/problem/1735" {
+        val input = """
+            2 7
+            3 5
+        """.trimIndent()
+        val expected = "31 35"
+
+        val actual = No1735.solve(BufferedReader(StringReader(input)))
+
+        actual shouldBe expected
+    }
+})
