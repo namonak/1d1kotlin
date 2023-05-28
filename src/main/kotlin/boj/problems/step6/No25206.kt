07503b4b77edb48a43d80ebf8191a562d9ed7184
namonak <namonak@gmail.com>
2023-05-28 20:37:16 +0900

    feat(boj): add test code and production code of No25206

---
+++ src/main/kotlin/boj/problems/step6/No25206.kt
@@ -0,0 +1,46 @@
+package boj.problems.step6
+
+import java.io.BufferedReader
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No25206.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No25206 {
+    private val gradeToScore = mapOf(
+        "A+" to 4.5,
+        "A0" to 4.0,
+        "B+" to 3.5,
+        "B0" to 3.0,
+        "C+" to 2.5,
+        "C0" to 2.0,
+        "D+" to 1.5,
+        "D0" to 1.0,
+        "F" to 0.0
+    )
+
+    fun solve(input: BufferedReader): String {
+        val totalScore = mutableListOf<Pair<Double, Double>>()
+        var result = 0.0
+
+        for (i in 1..20) {
+            val (_, credit, grade) = input.readLine().split(" ")
+            if (grade != "P") {
+                totalScore.add(Pair(credit.toDouble(), gradeToScore[grade] ?: 0.0))
+            }
+        }
+
+        totalScore.forEach { (credit, score) ->
+            result += credit * score
+        }
+
+        return String.format("%.6f", result / totalScore.sumOf { it.first })
+    }
+}
