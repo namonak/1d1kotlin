69d047d84fe6c1e9e86906e54711ce6e0b73fda2
namonak <namonak@gmail.com>
2023-07-03 12:05:17 +0900

    feat(boj): add test and production code of No11286

---
+++ src/main/kotlin/boj/problems/step28/No11286.kt
@@ -0,0 +1,48 @@
+package boj.problems.step28
+
+import java.io.BufferedReader
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No11286.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No11286 {
+    fun solve(input: BufferedReader): String {
+        val count = input.readLine().toInt()
+        val priorityQueue = PriorityQueue<Int> { o1, o2 ->
+            val abs1 = abs(o1)
+            val abs2 = abs(o2)
+            // 두 값의 값이 같을 경우, 부호를 비교해서 오름차순으로 정렬.
+            // 두 값이 다를 경우, 그냥 절댓값에서 숫자의 크기로 비교해서 오름차순으로 정렬
+            when (abs1) {
+                abs2 -> o1 - o2
+                else -> abs1 - abs2
+            }
+        }
+        val result = mutableListOf<Int>()
+
+        for (i in 0 until count) {
+            val number = input.readLine().toInt()
+            if (number == 0) {
+                if (priorityQueue.isEmpty()) {
+                    result.add(0)
+                } else {
+                    result.add(priorityQueue.poll())
+                }
+            } else {
+                priorityQueue.add(number)
+            }
+        }
+
+        return result.joinToString("\n")
+    }
+}
