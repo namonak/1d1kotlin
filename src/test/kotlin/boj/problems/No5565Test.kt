1816a87adcbaa73df707fa392532da6681d8f1d1
namonak <namonak@gmail.com>
2023-07-22 18:44:45 +0900

    feat(boj): add test and production code of No5565

---
+++ src/test/kotlin/boj/problems/No5565Test.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No5565Test : StringSpec({
+    "영수증 : https://www.acmicpc.net/problem/5565" {
+        val given = """
+            9850
+            1050
+            800
+            420
+            380
+            600
+            820
+            2400
+            1800
+            980
+        """.trimIndent()
+        val expected = 600
+
+        val actual = No5565.solve(BufferedReader(given.reader()))
+
+        actual shouldBe expected
+    }
+})
