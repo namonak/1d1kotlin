77fe17bfaead302d38b93c776e5fce05746638d1
namonak <namonak@gmail.com>
2023-08-18 12:25:31 +0900

    feat(boj): add test and production code of No10845

---
+++ src/main/kotlin/boj/problems/No10845.kt
@@ -0,0 +1,40 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No10845.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No10845 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val result = StringBuilder()
+        val queue = ArrayDeque<Int>()
+
+        repeat(n) {
+            executeOperation(input.readLine(), queue, result)
+        }
+
+        return result.toString().trimEnd()
+    }
+
+    private fun executeOperation(readLine: String, queue: ArrayDeque<Int>, result: StringBuilder) {
+        val st = readLine.split(" ")
+        when (st[0]) {
+            "push" -> queue.addLast(st[1].toInt())
+            "pop" -> result.append(if (queue.isEmpty()) "-1" else queue.removeFirst()).append("\n")
+            "size" -> result.append(queue.size.toString()).append("\n")
+            "empty" -> result.append(if (queue.isEmpty()) "1" else "0").append("\n")
+            "front" -> result.append(if (queue.isEmpty()) "-1" else queue.first()).append("\n")
+            "back" -> result.append(if (queue.isEmpty()) "-1" else queue.last()).append("\n")
+        }
+    }
+}
---
+++ src/test/kotlin/boj/problems/No10845Test.kt
@@ -0,0 +1,44 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No10845Test : StringSpec({
+    "큐 : https://www.acmicpc.net/problem/10845" {
+        val given = """
+            15
+            push 1
+            push 2
+            front
+            back
+            size
+            empty
+            pop
+            pop
+            pop
+            size
+            empty
+            pop
+            push 3
+            empty
+            front
+        """.trimIndent()
+        val expected = """
+            1
+            2
+            2
+            0
+            1
+            2
+            -1
+            0
+            1
+            -1
+            0
+            3
+        """.trimIndent()
+
+        No10845.solve(BufferedReader(given.reader())) shouldBe expected
+    }
+})
