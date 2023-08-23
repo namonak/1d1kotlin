464b8d45836a466ec1d82dd90b791fa47db5abea
namonak <namonak@gmail.com>
2023-08-23 10:31:16 +0900

    feat(boj): add test and production code of No10866

---
+++ src/main/kotlin/boj/problems/No10866.kt
@@ -0,0 +1,42 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No10866.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No10866 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val result = StringBuilder()
+        val deque = ArrayDeque<Int>()
+
+        repeat(n) {
+            executeOperation(input.readLine(), deque, result)
+        }
+
+        return result.toString().trimEnd()
+    }
+
+    private fun executeOperation(readLine: String, deque: ArrayDeque<Int>, result: StringBuilder) {
+        val st = readLine.split(" ")
+        when (st[0]) {
+            "push_front" -> deque.addFirst(st[1].toInt())
+            "push_back" -> deque.addLast(st[1].toInt())
+            "pop_front" -> result.append(if (deque.isEmpty()) "-1" else deque.removeFirst()).append("\n")
+            "pop_back" -> result.append(if (deque.isEmpty()) "-1" else deque.removeLast()).append("\n")
+            "size" -> result.append(deque.size.toString()).append("\n")
+            "empty" -> result.append(if (deque.isEmpty()) "1" else "0").append("\n")
+            "front" -> result.append(if (deque.isEmpty()) "-1" else deque.first()).append("\n")
+            "back" -> result.append(if (deque.isEmpty()) "-1" else deque.last()).append("\n")
+        }
+    }
+}
---
+++ src/test/kotlin/boj/problems/No10866Test.kt
@@ -0,0 +1,93 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No10866Test : StringSpec({
+    "덱 : https://www.acmicpc.net/problem/10866" {
+        val testCases = listOf(
+            """
+                15
+                push_back 1
+                push_front 2
+                front
+                back
+                size
+                empty
+                pop_front
+                pop_back
+                pop_front
+                size
+                empty
+                pop_back
+                push_front 3
+                empty
+                front
+            """.trimIndent()
+                to
+                    """
+                2
+                1
+                2
+                0
+                2
+                1
+                -1
+                0
+                1
+                -1
+                0
+                3
+                    """.trimIndent(),
+            """
+                22
+                front
+                back
+                pop_front
+                pop_back
+                push_front 1
+                front
+                pop_back
+                push_back 2
+                back
+                pop_front
+                push_front 10
+                push_front 333
+                front
+                back
+                pop_back
+                pop_back
+                push_back 20
+                push_back 1234
+                front
+                back
+                pop_back
+                pop_back
+            """.trimIndent()
+                to
+                    """
+                -1
+                -1
+                -1
+                -1
+                1
+                1
+                2
+                2
+                333
+                10
+                10
+                333
+                20
+                1234
+                1234
+                20
+                    """.trimIndent()
+        )
+
+        testCases.forEach { (given, expected) ->
+            No10866.solve(BufferedReader(given.reader())) shouldBe expected
+        }
+    }
+})
