590e5f308362c9cd4541868b2e0ed35dbf2fbde5
namonak <namonak@gmail.com>
2023-09-13 10:46:27 +0900

    feat(boj): add test and production code of No14487

---
+++ src/test/kotlin/boj/problems/No14487Test.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No14487Test : StringSpec({
+    "욱제는 효도쟁이야!! : https://www.acmicpc.net/problem/14487" {
+        val testCases = listOf(
+            """
+                5
+                1 6 5 2 4
+            """.trimIndent()
+                to
+                    12,
+            """
+                4
+                100 100 100 101
+            """.trimIndent()
+                to
+                    300
+        )
+
+        testCases.forEach { (given, expected) ->
+            No14487.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
