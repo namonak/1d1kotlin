fb634c0dc79cd0648d0d5538920901b7e5a55432
namonak <namonak@gmail.com>
2023-09-25 10:34:05 +0900

    feat(boj): add test and production code of No2108

---
+++ src/main/kotlin/boj/problems/No2108.kt
@@ -0,0 +1,42 @@
+package boj.problems
+
+import java.io.BufferedReader
+import kotlin.math.roundToInt
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    output.write(No2108.solve(input))
+
+    input.close()
+    output.flush()
+    output.close()
+}
+
+object No2108 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val numbers = mutableListOf<Int>()
+        val count = mutableMapOf<Int, Int>()
+
+        for (i in 0 until n) {
+            val number = input.readLine().toInt()
+            numbers.add(number)
+            count[number] = count.getOrDefault(number, 0) + 1
+        }
+
+        val sortedNumbers = numbers.sorted()
+        val average = sortedNumbers.average().roundToInt()
+        val median = sortedNumbers[n / 2]
+        val mode = count.filter { it.value == count.maxByOrNull { it.value }!!.value }.toList().sortedBy { it.first }
+        val range = sortedNumbers.last() - sortedNumbers.first()
+
+        return """
+            $average
+            $median
+            ${mode[if (mode.size == 1) 0 else 1].first}
+            $range
+        """.trimIndent()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No2108Test.kt
@@ -0,0 +1,66 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No2108Test : StringSpec({
+    "통계학 : https://www.acmicpc.net/problem/2108" {
+        val testCases = listOf(
+            """
+                5
+                1
+                3
+                8
+                -2
+                2
+            """.trimIndent()
+                to """
+                    2
+                    2
+                    1
+                    10
+                """.trimIndent(),
+            """
+                1
+                4000
+            """.trimIndent()
+                to """
+                    4000
+                    4000
+                    4000
+                    0
+                """.trimIndent(),
+            """
+                5
+                -1
+                -2
+                -3
+                -1
+                -2
+            """.trimIndent()
+                to """
+                    -2
+                    -2
+                    -1
+                    2
+                """.trimIndent(),
+            """
+                3
+                0
+                0
+                -1
+            """.trimIndent()
+                to """
+                    0
+                    0
+                    0
+                    1
+                """.trimIndent()
+
+        )
+
+        testCases.forEach { (given, expected) ->
+            No2108.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
