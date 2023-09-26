e8093664485ff3cebdfe8dce48ddc09462a98c7e
namonak <namonak@gmail.com>
2023-09-26 12:04:00 +0900

    fix: safely access Map values in TwoSum to address SonarCloud warning S6611

--- src/main/kotlin/leetcode/problems/lv0easy/TwoSum.kt
+++ src/main/kotlin/leetcode/problems/lv0easy/TwoSum.kt
@@ -1,12 +1,12 @@
 package leetcode.problems.lv0easy
 
 class TwoSum {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
+    fun twoSum(nums: IntArray, target: Int): IntArray? {
         val map = HashMap<Int, Int>()
         for (i in nums.indices) {
             val find = target - nums[i]
             if (map.containsKey(find)) {
-                return intArrayOf(map[find]!!, i)
+                return map[find]?.let { intArrayOf(it, i) }
             }
             map[nums[i]] = i
         }
