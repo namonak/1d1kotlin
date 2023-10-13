86e5375a5c6f568984da6c878f1d7214093448f2
namonak <namonak@gmail.com>
2023-10-13 15:37:00 +0900

    feat(boj): add test and production code of No11659

---
+++ src/main/kotlin/boj/problems/No11659.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() {
+    println(No11659.solve(System.`in`.bufferedReader()))
+}
+
+object No11659 {
+    fun solve(input: BufferedReader): String {
+        val (n, m) = input.readLine().split(" ").map { it.toInt() }
+        val numbers = input.readLine().split(" ").map { it.toInt() }
+        val dp = IntArray(n + 1) { 0 }
+        val sb = StringBuilder()
+
+        for (i in 1..n) {
+            dp[i] = dp[i - 1] + numbers[i - 1]
+        }
+
+        repeat(m) {
+            val (i, j) = input.readLine().split(" ").map { it.toInt() }
+            sb.append("${dp[j] - dp[i - 1]}\n")
+        }
+
+        return sb.toString().trimEnd()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No11659Test.kt
@@ -0,0 +1,23 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No11659Test : StringSpec({
+    "구간 합 구하기 4 : https://www.acmicpc.net/problem/11659" {
+        val given = """
+            5 3
+            5 4 3 2 1
+            1 3
+            2 4
+            5 5
+        """.trimIndent()
+        val expected = """
+            12
+            9
+            1
+        """.trimIndent()
+
+        No11659.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
