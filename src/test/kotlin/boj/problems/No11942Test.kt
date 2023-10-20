47e469542f522c32d2c9d682eba89936e09c75e0
namonak <namonak@gmail.com>
2023-10-20 15:14:18 +0900

    feat(boj): add test and production code of No11942

---
+++ src/test/kotlin/boj/problems/No11942Test.kt
@@ -0,0 +1,12 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No11942Test : StringSpec({
+    "고려대는 사랑입니다 : https://www.acmicpc.net/problem/11942" {
+        val expected = "고려대학교"
+
+        No11942.solve() shouldBe expected
+    }
+})
