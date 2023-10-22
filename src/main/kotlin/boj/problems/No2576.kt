5a13d851de4f69a94b57e953fa69d5bb961668a9
namonak <namonak@gmail.com>
2023-10-22 19:30:32 +0900

    feat(boj): add test and production code of No2576

---
+++ src/main/kotlin/boj/problems/No2576.kt
@@ -0,0 +1,24 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() {
+    println(No2576.solve(System.`in`.bufferedReader()))
+}
+
+object No2576 {
+    fun solve(input: BufferedReader): String {
+        var sum = 0
+        var min = Int.MAX_VALUE
+
+        for (i in 0 until 7) {
+            val n = input.readLine().toInt()
+            if (n % 2 == 1) {
+                sum += n
+                min = minOf(min, n)
+            }
+        }
+
+        return if (sum == 0) "-1" else "$sum\n$min"
+    }
+}
---
+++ src/test/kotlin/boj/problems/No2576Test.kt
@@ -0,0 +1,40 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No2576Test : StringSpec({
+    "홀수 : https://www.acmicpc.net/problem/2576" {
+        val testCases = listOf(
+            """
+                12
+                77
+                38
+                41
+                53
+                92
+                85
+            """.trimIndent()
+                to """
+                    256
+                    41
+                """.trimIndent(),
+            """
+                2
+                4
+                20
+                32
+                6
+                10
+                8
+            """.trimIndent()
+                to """
+                    -1
+                """.trimIndent(),
+        )
+
+        testCases.forEach { (input, output) ->
+            No2576.solve(input.byteInputStream().bufferedReader()) shouldBe output
+        }
+    }
+})
