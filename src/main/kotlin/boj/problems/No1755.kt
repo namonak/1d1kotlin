2f31e46611a3264936199654d3619b5cfa3f8cd3
namonak <namonak@gmail.com>
2023-10-27 17:43:48 +0900

    feat(boj): add test and production code of No1755

---
+++ src/main/kotlin/boj/problems/No1755.kt
@@ -0,0 +1,37 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() = println(No1755.solve(System.`in`.bufferedReader()))
+
+object No1755 {
+    fun solve(input: BufferedReader): String {
+        val numberToWord = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val (m, n) = input.readLine().split(" ").map { it.toInt() }
+
+        val numbers = (m..n).sortedWith(
+            compareBy { num ->
+                val tens = num / 10
+                val ones = num % 10
+
+                if (tens == 0) {
+                    return@compareBy numberToWord[ones]
+                }
+
+                "${numberToWord[tens]} ${numberToWord[ones]}"
+            }
+        )
+
+        val result = StringBuilder()
+        for (i in numbers.indices) {
+            result.append(numbers[i])
+            if (i % 10 == 9) {
+                result.append("\n")
+            } else {
+                result.append(" ")
+            }
+        }
+
+        return result.toString().trim()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No1755Test.kt
@@ -0,0 +1,17 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No1755Test : StringSpec({
+    "숫자놀이 : https://www.acmicpc.net/problem/1755" {
+        val given = "8 28"
+        val expected = """
+        8 9 18 15 14 19 11 17 16 13
+        12 10 28 25 24 21 27 26 23 22
+        20
+        """.trimIndent()
+
+        No1755.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
