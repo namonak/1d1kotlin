f4c9322d9ccf5033a3d2252d3182fc4df29d46cd
namonak <namonak@gmail.com>
2023-10-28 10:30:23 +0900

    feat(boj): add test and production code of No10820

---
+++ src/main/kotlin/boj/problems/No10820.kt
@@ -0,0 +1,25 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+fun main() = println(No10820.solve(System.`in`.bufferedReader()))
+
+object No10820 {
+    fun solve(input: BufferedReader): String {
+        val result = StringBuilder()
+        var line: String?
+        while (input.readLine().also { line = it } != null) {
+            val (lower, upper, number, space) = line!!.fold(listOf(0, 0, 0, 0)) { acc, c ->
+                when (c) {
+                    in 'a'..'z' -> acc.toMutableList().apply { set(0, acc[0] + 1) }
+                    in 'A'..'Z' -> acc.toMutableList().apply { set(1, acc[1] + 1) }
+                    in '0'..'9' -> acc.toMutableList().apply { set(2, acc[2] + 1) }
+                    ' ' -> acc.toMutableList().apply { set(3, acc[3] + 1) }
+                    else -> acc
+                }
+            }
+            result.append("$lower $upper $number $space\n")
+        }
+        return result.toString().trimEnd()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No10820Test.kt
@@ -0,0 +1,23 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No10820Test : StringSpec({
+    "문자열 분석 : https://www.acmicpc.net/problem/10820" {
+        val given = """
+            This is String
+            SPACE    1    SPACE
+             S a M p L e I n P u T     
+            0L1A2S3T4L5I6N7E8
+        """.trimIndent()
+        val expected = """
+            10 2 0 2
+            0 10 1 8
+            5 6 0 16
+            0 8 9 0
+        """.trimIndent()
+
+        No10820.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
