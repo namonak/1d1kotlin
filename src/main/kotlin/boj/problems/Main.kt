39817a7272b21659212c046f9df59d4df0e19cdd
namonak <namonak@gmail.com>
2023-11-18 22:42:29 +0900

    feat(boj): add test and production code of No1251

--- src/main/kotlin/boj/problems/Main.kt
+++ src/main/kotlin/boj/problems/Main.kt
@@ -1,3 +1,3 @@
 package boj.problems
 
-fun main() = println(No1475.solve(System.`in`.bufferedReader()))
+fun main() = println(No1251.solve(System.`in`.bufferedReader()))
---
+++ src/test/kotlin/boj/problems/No1251Test.kt
@@ -0,0 +1,13 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No1251Test : StringSpec({
+    "단어 나누기 : https://www.acmicpc.net/problem/1251" {
+        val given = "mobitel"
+        val expected = "bometil"
+
+        No1251.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
