2d10219f59a3a58939639894c8537752e7e565fe
namonak <namonak@gmail.com>
2023-12-27 19:40:10 +0900

    feat(boj): add test and production code of No2592

--- src/main/kotlin/boj/problems/Main.kt
+++ src/main/kotlin/boj/problems/Main.kt
@@ -1,3 +1,3 @@
 package boj.problems
 
-fun main() = println(No1380.solve(System.`in`.bufferedReader()))
+fun main() = println(No2592.solve(System.`in`.bufferedReader()))
---
+++ src/main/kotlin/boj/problems/No2592.kt
@@ -0,0 +1,19 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No2592 {
+    fun solve(input: BufferedReader): String {
+        val numbers = IntArray(10)
+        var sum = 0
+        repeat(10) {
+            val number = input.readLine().toInt()
+            numbers[it] = number
+            sum += number
+        }
+        val average = sum / 10
+        val mode = numbers.groupBy { it }.maxByOrNull { it.value.size }!!.key
+
+        return "$average\n$mode"
+    }
+}
