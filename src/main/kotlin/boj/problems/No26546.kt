66c317fce0b0ce9e9994655729c8775350537551
namonak <namonak@gmail.com>
2023-12-30 14:27:31 +0900

    feat(boj): add test and production code of No26546

---
+++ src/main/kotlin/boj/problems/No26546.kt
@@ -0,0 +1,21 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No26546 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val result = StringBuilder()
+
+        repeat(n) {
+            val line = input.readLine().split(" ")
+            val s = line[0]
+            val start = line[1].toInt()
+            val end = line[2].toInt()
+
+            result.appendLine(s.substring(0, start) + s.substring(end))
+        }
+
+        return result.toString().trimEnd()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No26546Test.kt
@@ -0,0 +1,22 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No26546Test : StringSpec({
+    "Reverse : https://www.acmicpc.net/problem/26546" {
+        val given = """
+            3
+            COMPUTER 1 3
+            SCIENCE 3 7
+            RULES 3 4
+        """.trimIndent()
+        val expected = """
+            CPUTER
+            SCI
+            RULS
+        """.trimIndent()
+
+        No26546.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
