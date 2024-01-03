1f841531bc3b8e9755f13a8ee68484e1b4405553
namonak <namonak@gmail.com>
2024-01-03 13:14:21 +0900

    feat(boj): add test and production code of No10931

---
+++ src/main/kotlin/boj/problems/No10931.kt
@@ -0,0 +1,12 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No10931 {
+    fun solve(input: BufferedReader): String {
+        val given = input.readLine()
+        val messageDigest = java.security.MessageDigest.getInstance("SHA-384")
+        val hash = messageDigest.digest(given.toByteArray())
+        return hash.joinToString("") { "%02x".format(it) }
+    }
+}
