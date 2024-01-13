4753ebf073857e7ba75135c74cda3e235896766e
namonak <namonak@gmail.com>
2024-01-13 13:59:34 +0900

    feat(boj): add test and production code of No14581

---
+++ src/main/kotlin/boj/problems/No14581.kt
@@ -0,0 +1,14 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No14581 {
+    fun solve(input: BufferedReader): String {
+        val id = input.readLine()
+        return """
+            :fan::fan::fan:
+            :fan::$id::fan:
+            :fan::fan::fan:
+        """.trimIndent()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No14581Test.kt
@@ -0,0 +1,25 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No14581Test : StringSpec({
+    "팬들에게 둘러싸인 홍준 : https://www.acmicpc.net/problem/14581" {
+        val testCases = listOf(
+            "appa" to """
+                :fan::fan::fan:
+                :fan::appa::fan:
+                :fan::fan::fan:
+            """.trimIndent(),
+            "h0ngjun7" to """
+                :fan::fan::fan:
+                :fan::h0ngjun7::fan:
+                :fan::fan::fan:
+            """.trimIndent()
+        )
+
+        testCases.forEach { (given, expected) ->
+            No14581.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
