81b257e3a60e287dbbce28c5b5d8e0d2e8fb4390
namonak <namonak@gmail.com>
2024-01-14 20:58:45 +0900

    feat(boj): add test and production code of No2193

---
+++ src/main/kotlin/boj/problems/No2193.kt
@@ -0,0 +1,17 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No2193 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val dp = Array(n + 1) { LongArray(2) }
+        dp[1][0] = 0
+        dp[1][1] = 1
+        for (i in 2..n) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
+            dp[i][1] = dp[i - 1][0]
+        }
+        return (dp[n][0] + dp[n][1]).toString()
+    }
+}
