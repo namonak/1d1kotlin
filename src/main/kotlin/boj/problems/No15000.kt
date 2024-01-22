c79efe4d9bdc183c6afcb205e86ceca0968c36f4
namonak <namonak@gmail.com>
2024-01-22 10:58:55 +0900

    feat(boj): add test and production code of No15000

---
+++ src/main/kotlin/boj/problems/No15000.kt
@@ -0,0 +1,10 @@
+package boj.problems
+
+import java.io.BufferedReader
+import java.util.Locale
+
+object No15000 {
+    fun solve(input: BufferedReader): String {
+        return input.readLine().uppercase(Locale.getDefault())
+    }
+}
---
+++ src/test/kotlin/boj/problems/No15000Test.kt
@@ -0,0 +1,18 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No15000Test : StringSpec({
+    "CAPS : https://www.acmicpc.net/problem/15000" {
+        val testCases = listOf(
+            "alert" to "ALERT",
+            "earthisunderattack" to "EARTHISUNDERATTACK",
+            "canyoupickupsomegroceries" to "CANYOUPICKUPSOMEGROCERIES"
+        )
+
+        testCases.forEach { (given, expected) ->
+            No15000.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
