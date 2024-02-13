c5c4a660f4ffa51a9065d0da3317b83b8edbecdb
namonak <namonak@gmail.com>
2024-02-13 20:37:44 +0900

    feat(boj): add test and production code of No5337

---
+++ src/main/kotlin/boj/problems/No5337.kt
@@ -0,0 +1,9 @@
+package boj.problems
+
+object No5337 {
+    fun solve() = """
+        .  .   .
+        |  | _ | _. _ ._ _  _
+        |/\|(/.|(_.(_)[ | )(/.
+    """.trimIndent()
+}
---
+++ src/test/kotlin/boj/problems/No5337Test.kt
@@ -0,0 +1,16 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No5337Test : StringSpec({
+    "웰컴 : https://www.acmicpc.net/problem/5337" {
+        val expected = """
+            .  .   .
+            |  | _ | _. _ ._ _  _
+            |/\|(/.|(_.(_)[ | )(/.
+        """.trimIndent()
+
+        No5337.solve() shouldBe expected
+    }
+})
