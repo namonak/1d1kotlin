ff2ae01b2cb8eeebee7acd64ed936d390088a8e9
namonak <namonak@gmail.com>
2024-02-15 10:47:17 +0900

    feat(boj): add test and production code of No14495

---
+++ src/main/kotlin/boj/problems/No14495.kt
@@ -0,0 +1,26 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No14495 {
+    private const val MAX_INPUT = 116
+    private val memo = Array(MAX_INPUT + 1) { -1L }
+
+    fun solve(input: BufferedReader): String {
+        memo[1] = 1
+        memo[2] = 1
+        memo[3] = 1
+
+        return fibonacci(input.readLine().toInt()).toString()
+    }
+
+    private fun fibonacci(n: Int): Long {
+        if (memo[n] != -1L) {
+            return memo[n]
+        }
+
+        memo[n] = fibonacci(n - 1) + fibonacci(n - 3)
+
+        return memo[n]
+    }
+}
---
+++ src/test/kotlin/boj/problems/No14495Test.kt
@@ -0,0 +1,13 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No14495Test : StringSpec({
+    "피보나치 비스무리한 수열 : https://www.acmicpc.net/problem/14495" {
+        val given = "10"
+        val expected = "19"
+
+        No14495.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
