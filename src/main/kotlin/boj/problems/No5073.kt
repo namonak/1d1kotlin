f550c44d543279add76729e3b5a466ad430a2e8a
namonak <namonak@gmail.com>
2024-02-20 10:50:00 +0900

    feat(boj): add test and production code of No5073

---
+++ src/main/kotlin/boj/problems/No5073.kt
@@ -0,0 +1,33 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+object No5073 {
+    fun solve(input: BufferedReader): String {
+        val result = StringBuilder()
+        while (true) {
+            val (a, b, c) = input.readLine().split(" ").map { it.toInt() }
+            if (a == 0 && b == 0 && c == 0) break
+            result.append(
+                classifyTriangle(a, b, c)
+            ).append("\n")
+        }
+        return result.toString().trim()
+    }
+
+    private fun classifyTriangle(a: Int, b: Int, c: Int): String {
+        return when {
+            // 어떤 두 변의 합도 세 번째 변보다 커야 한다는 조건을 만족하지 못하는 경우
+            a + b <= c || a + c <= b || b + c <= a -> "Invalid"
+
+            // 세 변의 길이가 모두 같은 경우
+            a == b && b == c -> "Equilateral"
+
+            // 두 변의 길이만 같은 경우
+            a == b || b == c || a == c -> "Isosceles"
+
+            // 위의 모든 조건이 아니라면, 세 변의 길이가 모두 다른 경우이다.
+            else -> "Scalene"
+        }
+    }
+}
---
+++ src/test/kotlin/boj/problems/No5073Test.kt
@@ -0,0 +1,24 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No5073Test : StringSpec({
+    "삼각형과 세 변 : https://www.acmicpc.net/problem/5073" {
+        val given = """
+            7 7 7
+            6 5 4
+            3 2 5
+            6 2 6
+            0 0 0
+        """.trimIndent()
+        val expected = """
+            Equilateral
+            Scalene
+            Invalid
+            Isosceles
+        """.trimIndent()
+
+        No5073.solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
