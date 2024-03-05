48f5f4846d26c5e25cde254ba5d6e0e7d71a26c4
namonak <namonak@gmail.com>
2024-03-05 16:09:49 +0900

    feat(boj): add test and production code of No2630

---
+++ src/main/kotlin/boj/problems/No2630.kt
@@ -0,0 +1,39 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No2630 {
+    private var whiteCount = 0
+    private var blueCount = 0
+    private lateinit var paper: Array<IntArray>
+
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        paper = Array(n) { input.readLine().split(" ").map { it.toInt() }.toIntArray() }
+        checkAndSplitPaper(0, 0, n)
+        return "$whiteCount\n$blueCount"
+    }
+
+    private fun checkAndSplitPaper(x: Int, y: Int, size: Int) {
+        if (isAllSameColor(x, y, size)) {
+            if (paper[x][y] == 0) whiteCount++
+            else blueCount++
+            return
+        }
+        val newSize = size / 2
+        checkAndSplitPaper(x, y, newSize)
+        checkAndSplitPaper(x, y + newSize, newSize)
+        checkAndSplitPaper(x + newSize, y, newSize)
+        checkAndSplitPaper(x + newSize, y + newSize, newSize)
+    }
+
+    private fun isAllSameColor(x: Int, y: Int, size: Int): Boolean {
+        val color = paper[x][y]
+        for (i in x until x + size) {
+            for (j in y until y + size) {
+                if (paper[i][j] != color) return false
+            }
+        }
+        return true
+    }
+}
---
+++ src/test/kotlin/boj/problems/No2630Test.kt
@@ -0,0 +1,26 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No2630Test : StringSpec({
+    "색종이 만들기 : https://www.acmicpc.net/problem/2630" {
+        val given = """
+            8
+            1 1 0 0 0 0 1 1
+            1 1 0 0 0 0 1 1
+            0 0 0 0 1 1 0 0
+            0 0 0 0 1 1 0 0
+            1 0 0 0 1 1 1 1
+            0 1 0 0 1 1 1 1
+            0 0 1 1 1 1 1 1
+            0 0 1 1 1 1 1 1
+        """.trimIndent()
+        val expected = """
+            9
+            7
+        """.trimIndent()
+
+        No2630().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
