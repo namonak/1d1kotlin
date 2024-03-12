1d1a3fd6917f214a035801abd83569c7228a1c18
namonak <namonak@gmail.com>
2024-03-12 10:56:50 +0900

    feat(boj): add test and production code of No9184

---
+++ src/main/kotlin/boj/problems/No9184.kt
@@ -0,0 +1,44 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+const val MAX = 21
+
+class No9184 {
+    // 문제의 조건에 따라, a, b, c의 값이 각각 0 이하이거나 20 초과인 경우는 결과가 정해져 있기 때문에 MAX(21)로만 설정
+    val dp = Array(MAX) { Array(MAX) { IntArray(MAX) } }
+
+    fun solve(input: BufferedReader): String {
+        val result = StringBuilder()
+
+        while (true) {
+            val (a, b, c) = input.readLine().split(" ").map { it.toInt() }
+            if (a == -1 && b == -1 && c == -1) break
+
+            result.append("w($a, $b, $c) = ${w(a, b, c)}\n")
+        }
+
+        return result.toString().trimEnd()
+    }
+
+    private fun w(a: Int, b: Int, c: Int): Int {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1
+        }
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20)
+        }
+
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c]
+        }
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+            return dp[a][b][c]
+        }
+
+        dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+        return dp[a][b][c]
+    }
+}
---
+++ src/test/kotlin/boj/problems/No9184Test.kt
@@ -0,0 +1,26 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No9184Test : StringSpec({
+    "신나는 함수 실행 : https://www.acmicpc.net/problem/9184" {
+        val given = """
+            1 1 1
+            2 2 2
+            10 4 6
+            50 50 50
+            -1 7 18
+            -1 -1 -1
+        """.trimIndent()
+        val expected = """
+            w(1, 1, 1) = 2
+            w(2, 2, 2) = 4
+            w(10, 4, 6) = 523
+            w(50, 50, 50) = 1048576
+            w(-1, 7, 18) = 1
+        """.trimIndent()
+
+        No9184().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
