efed9ecbc66bc5ba4b128351965837308194af1c
namonak <namonak@gmail.com>
2024-03-20 17:05:45 +0900

    feat(boj): add test and production code of No9656

--- src/main/kotlin/boj/problems/Main.kt
+++ src/main/kotlin/boj/problems/Main.kt
@@ -1,3 +1,3 @@
 package boj.problems
 
-fun main() = println(No16479().solve(System.`in`.bufferedReader()))
+fun main() = println(No9656().solve(System.`in`.bufferedReader()))
---
+++ src/test/kotlin/boj/problems/No9656Test.kt
@@ -0,0 +1,13 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No9656Test : StringSpec({
+    "돌 게임 2 : https://www.acmicpc.net/problem/9656" {
+        val given = "4"
+        val expected = "SK"
+
+        No9656().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
