9f975ea0b93eff3c7758730b29f10a5c2baf0513
namonak <namonak@gmail.com>
2024-04-12 15:17:16 +0900

    feat(boj): add test and production code of No9237

---
+++ src/main/kotlin/boj/problems/No9237.kt
@@ -0,0 +1,15 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No9237 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val trees = input.readLine().split(" ").map { it.toInt() }.sortedDescending()
+        var max = 0
+        for (i in trees.indices) {
+            max = maxOf(max, trees[i] + i + 2)
+        }
+        return max.toString()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No9237Test.kt
@@ -0,0 +1,25 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No9237Test : StringSpec({
+    "이장님 초대 : https://www.acmicpc.net/problem/9237" {
+        val testCases = listOf(
+            """
+                4
+                2 3 4 3
+            """.trimIndent()
+                to "7",
+            """
+                6
+                39 38 9 35 39 20
+            """.trimIndent()
+                to "42"
+        )
+
+        testCases.forEach { (given, expected) ->
+            No9237().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
