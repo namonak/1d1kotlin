47afe3c41e2e758f1e71b81b09961dad40c87314
namonak <namonak@gmail.com>
2024-05-24 21:43:43 +0900

    feat(boj): add test and production code of No5341

---
+++ src/main/kotlin/boj/problems/No5341.kt
@@ -0,0 +1,20 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No5341 {
+    fun solve(input: BufferedReader) = with(input) {
+        val sb = StringBuilder()
+        while (true) {
+            val n = readLine().toInt()
+            if (n == 0) break
+            var sum = 0
+
+            for (i in 1..n) {
+                sum += i
+            }
+            sb.appendLine(sum)
+        }
+        sb.toString().trimEnd()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No5341Test.kt
@@ -0,0 +1,20 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No5341Test : StringSpec({
+    "Pyramids : https://www.acmicpc.net/problem/5341" {
+        val given = """
+            4
+            6
+            0
+        """.trimIndent()
+        val expected = """
+            10
+            21
+        """.trimIndent()
+
+        No5341().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+    }
+})
