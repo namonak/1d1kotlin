9ed315dda5d5c554ece763d949efadca77f8a977
namonak <namonak@gmail.com>
2024-07-04 17:09:40 +0900

    feat(boj): add test and production code of No30958

---
+++ src/main/kotlin/boj/problems/No30958.kt
@@ -0,0 +1,12 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No30958 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val s = input.readLine().filterNot { it in " ,." }
+        val sortedFrequency = s.groupingBy { it }.eachCount().values.sortedDescending()
+        return sortedFrequency.first().toString()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No30958Test.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No30958Test : StringSpec({
+    "서울사이버대학을 다니고 : https://www.acmicpc.net/problem/30958" {
+        val testCases = listOf(
+            """
+                255
+                my era of success began with seoul cyber university, since attending seoul cyber university, more companies have been seeking me out, my life has changed since i went to seoul cyber university, the strategy that changes the future, seoul cyber university.
+            """.trimIndent() to "34",
+            """
+                1
+                m
+            """.trimIndent() to "1",
+            """
+                51
+                my era of success began with seoul cyber university
+            """.trimIndent() to "6"
+        )
+
+        testCases.forEach { (given, expected) ->
+            No30958().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
