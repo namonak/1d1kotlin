9a418920c66b04cfd07d7926aa19a724eb968396
namonak <namonak@gmail.com>
2024-07-22 13:33:07 +0900

    feat(boj): add test and production code of No5618

---
+++ src/main/kotlin/boj/problems/No5618.kt
@@ -0,0 +1,38 @@
+package boj.problems
+
+import java.io.BufferedReader
+import kotlin.math.sqrt
+
+class No5618 {
+    fun solve(input: BufferedReader): String {
+        // 'n' is not used
+        input.readLine().toInt()
+        val numbers = input.readLine().split(" ").map { it.toInt() }
+        val gcd = findGCD(numbers)
+        val divisors = getDivisors(gcd)
+
+        return divisors.joinToString("\n")
+    }
+
+    private fun findGCD(numbers: List<Int>): Int {
+        return numbers.reduce { acc, num -> gcd(acc, num) }
+    }
+
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+
+    private fun getDivisors(gcd: Int): List<Int> {
+        val divisors = mutableListOf<Int>()
+
+        for (i in 1..sqrt(gcd.toDouble()).toInt()) {
+            if (gcd % i == 0) {
+                divisors.add(i)
+                if (i != gcd / i) {
+                    divisors.add(gcd / i)
+                }
+            }
+        }
+        return divisors.sorted()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No5618Test.kt
@@ -0,0 +1,41 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No5618Test : StringSpec({
+    "공약수 : https://www.acmicpc.net/problem/5618" {
+        val testCases = listOf(
+            """
+                2
+                75 125
+            """.trimIndent() to
+                """
+                    1
+                    5
+                    25
+                """.trimIndent(),
+            """
+                3
+                110 22 88
+            """.trimIndent() to
+                """
+                    1
+                    2
+                    11
+                    22
+                """.trimIndent(),
+            """
+                3
+                66 11 3
+            """.trimIndent() to
+                """
+                    1
+                """.trimIndent()
+        )
+
+        testCases.forEach { (given, expected) ->
+            No5618().solve(given.byteInputStream().bufferedReader()) shouldBe expected
+        }
+    }
+})
