858f7b49384438ff08df0759e906c07a3009fe35
namonak <namonak@gmail.com>
2024-07-22 14:14:39 +0900

    test: update test cases for problem 13420 with additional scenarios
     - Added new test cases for multiplication, addition, subtraction, and division
     - Included correct and incorrect answer checks for various arithmetic operations

--- src/test/kotlin/boj/problems/No13420Test.kt
+++ src/test/kotlin/boj/problems/No13420Test.kt
@@ -6,17 +6,25 @@ import io.kotest.matchers.shouldBe
 class No13420Test : StringSpec({
     "사칙연산 : https://www.acmicpc.net/problem/13420" {
         val given = """
-            4
-            3 * 2 = 6
+            8
+            3 * 0 = 0
+            3 * 2 = 7
+            11 + 11 = 22
             11 + 11 = 11
             7 - 9 = -2
-            3 * 0 = 0
+            7 - 9 = 1
+            9 / 3 = 3
+            9 / 3 = 4
         """.trimIndent()
         val expected = """
             correct
             wrong answer
             correct
+            wrong answer
             correct
+            wrong answer
+            correct
+            wrong answer
         """.trimIndent()
 
         No13420().solve(given.byteInputStream().bufferedReader()) shouldBe expected
