41ec21ddd37b2f20cc09041ac65a8c641ffcef74
namonak <namonak@gmail.com>
2024-09-12 15:55:02 +0900

    refactor(boj): refactor No10828 class and update test cases
     - Refactored No10828 from an object to a class for better flexibility and testability.
     - Removed unnecessary BufferedWriter in the main function.
     - Updated No10828Test to use inline test cases instead of external file inputs.
     - Trimmed the result to remove trailing newlines in the solve function.

--- src/main/kotlin/boj/problems/step19/No10828.kt
+++ src/main/kotlin/boj/problems/step19/No10828.kt
@@ -1,13 +1,10 @@
 package boj.problems.step19
 
 import java.io.BufferedReader
-import java.io.BufferedWriter
 import java.io.IOException
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.util.StringTokenizer
 
-object No10828 {
+class No10828 {
     @Throws(IOException::class)
     fun solve(input: BufferedReader): String {
         val n = input.readLine().toInt()
@@ -16,7 +13,7 @@ object No10828 {
         for (i in 0 until n) {
             executeOperation(input.readLine(), stack, result)
         }
-        return result.toString()
+        return result.toString().trimEnd()
     }
 
     private fun executeOperation(operation: String, stack: Stack, result: StringBuilder) {
@@ -68,13 +65,4 @@ object No10828 {
             return arr[size - 1]
         }
     }
-}
-
-fun main() {
-    val input = BufferedReader(InputStreamReader(System.`in`))
-    val output = BufferedWriter(OutputStreamWriter(System.out))
-    output.write(No10828.solve(input))
-    input.close()
-    output.flush()
-    output.close()
 }
--- src/test/kotlin/boj/problems/step19/No10828Test.kt
+++ src/test/kotlin/boj/problems/step19/No10828Test.kt
@@ -1,30 +1,62 @@
 package boj.problems.step19
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
-import java.io.File
+import java.io.BufferedReader
 
 class No10828Test : StringSpec({
-    "스택" {
-        val path = File(".")
-
+    "스택 : https://www.acmicpc.net/problem/10828" {
         val testCases = listOf(
-            row("No10828_input_1.txt", "No10828_output_1.txt"),
-            row("No10828_input_2.txt", "No10828_output_2.txt")
+            """
+                14
+                push 1
+                push 2
+                top
+                size
+                empty
+                pop
+                pop
+                pop
+                size
+                empty
+                pop
+                push 3
+                empty
+                top
+            """.trimIndent() to """
+                2
+                2
+                0
+                2
+                1
+                -1
+                0
+                1
+                -1
+                0
+                3
+            """.trimIndent(),
+            """
+                7
+                pop
+                top
+                push 123
+                top
+                pop
+                top
+                pop
+            """.trimIndent() to """
+                -1
+                -1
+                123
+                123
+                -1
+                -1
+            """.trimIndent()
         )
 
-        testCases.forEach { (input, output) ->
-            // given
-            val given = File(path, "src/test/kotlin/boj/problems/step19/$input").bufferedReader()
-            val expected = File(path, "src/test/kotlin/boj/problems/step19/$output").readText()
-
-            // when
-            val actual = No10828.solve(given)
-
-            // then
-            actual shouldBe expected
-            given.close()
+        testCases.forEach { (given, expected) ->
+            No10828().solve(BufferedReader(given.reader())) shouldBe expected
         }
     }
 })
