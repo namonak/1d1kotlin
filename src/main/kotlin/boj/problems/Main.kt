3d33facf01450d907cb10012ffbff59b249b0c1a
namonak <namonak@gmail.com>
2024-10-18 17:57:00 +0900

    feat(boj): add test and production code of No2447 (Divide and Conquer)

--- src/main/kotlin/boj/problems/Main.kt
+++ src/main/kotlin/boj/problems/Main.kt
@@ -1,3 +1,3 @@
 package boj.problems
 
-fun main() = println(No1074().solve(System.`in`.bufferedReader()))
+fun main() = println(No2447().solve(System.`in`.bufferedReader()))
---
+++ src/main/kotlin/boj/problems/No2447.kt
@@ -0,0 +1,34 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No2447 {
+    private lateinit var stars: Array<CharArray>
+
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+
+        stars = Array(n) { CharArray(n) { ' ' } }
+
+        drawStars(0, 0, n)
+
+        return stars.joinToString("\n") { it.joinToString("") }
+    }
+
+    private fun drawStars(x: Int, y: Int, size: Int) {
+        if (size == 1) {
+            stars[x][y] = '*'
+            return
+        }
+
+        val newSize = size / 3
+        drawStars(x, y, newSize)
+        drawStars(x, y + newSize, newSize)
+        drawStars(x, y + newSize * 2, newSize)
+        drawStars(x + newSize, y, newSize)
+        drawStars(x + newSize, y + newSize * 2, newSize)
+        drawStars(x + newSize * 2, y, newSize)
+        drawStars(x + newSize * 2, y + newSize, newSize)
+        drawStars(x + newSize * 2, y + newSize * 2, newSize)
+    }
+}
