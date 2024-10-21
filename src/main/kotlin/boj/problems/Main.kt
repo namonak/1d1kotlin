bfd2c2570b3071bc6a461ba2f3886f2072f73023
namonak <namonak@gmail.com>
2024-10-21 17:27:21 +0900

    feat(boj): add test and production code of No15652 (Backtracking)

--- src/main/kotlin/boj/problems/Main.kt
+++ src/main/kotlin/boj/problems/Main.kt
@@ -1,3 +1,3 @@
 package boj.problems
 
-fun main() = println(No15651().solve(System.`in`.bufferedReader()))
+fun main() = println(No15652().solve(System.`in`.bufferedReader()))
---
+++ src/main/kotlin/boj/problems/No15652.kt
@@ -0,0 +1,35 @@
+package boj.problems
+
+import java.io.BufferedReader
+import java.util.Scanner
+
+class No15652 {
+    private var n: Int = 0
+    private var m: Int = 0
+    private lateinit var arr: IntArray
+    private val result = StringBuilder()
+
+    fun solve(input: BufferedReader): String {
+        val scanner = Scanner(input.readLine())
+
+        n = scanner.nextInt()
+        m = scanner.nextInt()
+        arr = IntArray(m) { 0 }
+
+        dfs(1, 0)
+
+        return result.toString().trimEnd()
+    }
+
+    private fun dfs(start: Int, depth: Int) {
+        if (depth == m) {
+            result.append(arr.joinToString(" ") + "\n")
+            return
+        }
+
+        for (i in start..n) {
+            arr[depth] = i
+            dfs(i, depth + 1)
+        }
+    }
+}
