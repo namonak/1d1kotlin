cdf580104ca181dda9fadde07659483d64d0fd25
namonak <namonak@gmail.com>
2024-12-04 15:55:13 +0900

    feat(boj): add test and production code of No3273 (Sorting)

---
+++ src/main/kotlin/boj/problems/No3273.kt
@@ -0,0 +1,21 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No3273 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().toInt()
+        val numbers = input.readLine().split(" ").map { it.toInt() }
+        val x = input.readLine().toInt()
+
+        val set = numbers.toSet()
+        var count = 0
+        for (i in numbers) {
+            if (set.contains(x - i)) {
+                count++
+            }
+        }
+
+        return (count / 2).toString()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No3273Test.kt
@@ -0,0 +1,18 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No3273Test : StringSpec({
+    "두 수의 합 : https://www.acmicpc.net/problem/3273" {
+        val given = """
+            9
+            5 12 7 10 9 1 2 3 11
+            13
+        """.trimIndent()
+        val expected = "3"
+
+        No3273().solve(BufferedReader(given.reader())) shouldBe expected
+    }
+})
