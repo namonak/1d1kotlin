7ae03fee67b3187b16a967d468cef44f33b75bd3
namonak <namonak@gmail.com>
2024-12-26 20:03:05 +0900

    feat(boj): add test and production code of No1340 (Implementation)

---
+++ src/main/kotlin/boj/problems/No1340.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import java.io.BufferedReader
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+class No1340 {
+    fun solve(input: BufferedReader): String {
+        val inputLine = input.readLine()
+        val formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy HH:mm")
+        val dateTime = LocalDateTime.parse(inputLine, formatter)
+        val year = dateTime.year
+        val daysInYear = if (isLeapYear(year)) 366 else 365
+        val totalMinutesInYear = daysInYear * 24 * 60
+
+        val startOfYear = LocalDateTime.of(year, 1, 1, 0, 0)
+        val elapsedMinutes = ChronoUnit.MINUTES.between(startOfYear, dateTime).toDouble()
+        val progressPercentage = (elapsedMinutes / totalMinutesInYear) * 100
+
+        return String.format("%.9f", progressPercentage)
+    }
+
+    private fun isLeapYear(year: Int): Boolean {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+    }
+}
---
+++ src/test/kotlin/boj/problems/No1340Test.kt
@@ -0,0 +1,22 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No1340Test : StringSpec({
+    "Year Progress Test" {
+        val testCases = listOf(
+            "May 10, 1981 00:31" to "35.348363774733635",
+            "January 01, 2008 00:00" to "0.0",
+            "December 31, 2007 23:59" to "99.99980974124807",
+            "July 02, 2007 12:00" to "50.0",
+            "July 02, 2008 00:00" to "50.0",
+            "January 31, 1900 00:47" to "8.228120243531203"
+        )
+
+        testCases.forEach { (given, expected) ->
+            No1340().solve(BufferedReader(given.reader())) shouldBe expected
+        }
+    }
+})
