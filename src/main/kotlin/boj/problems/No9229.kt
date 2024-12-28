00495e0897810ed210e0f8ed71fc5867f19a0f39
namonak <namonak@gmail.com>
2024-12-28 19:05:22 +0900

    feat(boj): add test and production code of No9229 (String, Brute-force)

---
+++ src/main/kotlin/boj/problems/No9229.kt
@@ -0,0 +1,50 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No9229 {
+    fun solve(input: BufferedReader): String {
+        val result = StringBuilder()
+
+        while (true) {
+            val testCase = mutableListOf<String>()
+            while (true) {
+                val line = input.readLine()?.trim() ?: "#"
+                if (line == "#") {
+                    if (testCase.isNotEmpty()) {
+                        result.append(if (isWordLadder(testCase)) "Correct" else "Incorrect").append('\n')
+                    }
+                    break
+                }
+                testCase.add(line)
+            }
+
+            if (testCase.isEmpty()) break
+        }
+
+        return result.toString().trim()
+    }
+
+    private fun isWordLadder(words: List<String>): Boolean {
+        for (i in 0 until words.size - 1) {
+            if (!isSingleChange(words[i], words[i + 1])) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun isSingleChange(word1: String, word2: String): Boolean {
+        if (word1.length != word2.length) return false
+
+        var diffCount = 0
+        for (i in word1.indices) {
+            if (word1[i] != word2[i]) {
+                diffCount++
+                if (diffCount > 1) return false
+            }
+        }
+
+        return diffCount == 1
+    }
+}
---
+++ src/test/kotlin/boj/problems/No9229Test.kt
@@ -0,0 +1,43 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No9229Test : StringSpec({
+    "단어 사다리 : https://www.acmicpc.net/problem/9229" {
+        val given = """
+            BARK
+            BARE
+            #
+            BEAK
+            BRAK
+            BRAD
+            BEAD
+            #
+            BEAK
+            BEAD
+            BEND
+            LEND
+            LAND
+            LANE
+            LAKE
+            #
+            MAKE
+            BAKE
+            BONK
+            BONE
+            BANE
+            #
+            #
+        """.trimIndent()
+        val expected = """
+            Correct
+            Correct
+            Correct
+            Incorrect
+        """.trimIndent()
+
+        No9229().solve(BufferedReader(given.reader())) shouldBe expected
+    }
+})
