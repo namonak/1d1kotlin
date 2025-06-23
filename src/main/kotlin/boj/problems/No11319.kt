54b16f1b7403ecfb6cdf656d48bf3a53cee92aa8
namonak <namonak@gmail.com>
2025-06-23 15:21:15 +0900

    feat(boj): add test and production code of No11319 (String)

---
+++ src/main/kotlin/boj/problems/No11319.kt
@@ -0,0 +1,27 @@
+package boj.problems
+
+import java.io.BufferedReader
+
+class No11319 {
+    fun solve(input: BufferedReader): String {
+        val vowelSet = setOf('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
+        val s = input.readLine().toInt()
+        val result = StringBuilder()
+
+        repeat(s) {
+            val line = input.readLine()
+            var vowelCount = 0
+            var consonantCount = 0
+
+            for (char in line) {
+                if (!char.isLetter()) continue
+                if (char in vowelSet) vowelCount++
+                else consonantCount++
+            }
+
+            result.append("$consonantCount $vowelCount\n")
+        }
+
+        return result.toString().trim()
+    }
+}
---
+++ src/test/kotlin/boj/problems/No11319Test.kt
@@ -0,0 +1,24 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No11319Test : StringSpec({
+    "Count Me In : https://www.acmicpc.net/problem/11319" {
+        val given = """
+            3
+            You can win this thing
+            May be too optimistic
+            Just try to have fun
+        """.trimIndent()
+        val expected = """
+            12 6
+            10 8
+            11 5
+        """.trimIndent()
+
+        val actual = No11319().solve(BufferedReader(given.reader()))
+        actual shouldBe expected
+    }
+})
