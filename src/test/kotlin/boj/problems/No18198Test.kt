8ff3153755b01fa585d8fa3de29b8de6af05e211
namonak <namonak@gmail.com>
2025-07-22 10:58:40 +0900

    fix(boj): fix expected result in test case of No18198Test

--- src/test/kotlin/boj/problems/No18198Test.kt
+++ src/test/kotlin/boj/problems/No18198Test.kt
@@ -8,7 +8,7 @@ class No18198Test : StringSpec({
     "Basketball One-on-One : https://www.acmicpc.net/problem/18198" {
         val testCases = listOf(
             "A2B1A2B2A1A2A2A2" to "A",
-            "A2B2A1B2A2B1A2B2A1B2A1A1B1A1A2" to "B"
+            "A2B2A1B2A2B1A2B2A1B2A1A1B1A1A2" to "A"
         )
 
         testCases.forEach { (given, expected) ->
