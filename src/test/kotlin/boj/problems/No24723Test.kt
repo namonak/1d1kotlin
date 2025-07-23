eb3a6164b889ef4303c6590a47dc8b8a8bd98d25
namonak <namonak@gmail.com>
2025-07-23 11:30:12 +0900

    feat(boj): add test and production code of No24723 (Math)

---
+++ src/test/kotlin/boj/problems/No24723Test.kt
@@ -0,0 +1,19 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.io.BufferedReader
+
+class No24723Test : StringSpec({
+    "녹색거탑 : https://www.acmicpc.net/problem/24723" {
+        val testCases = listOf(
+            "1" to "1",
+            "2" to "4",
+        )
+
+        testCases.forEach { (given, expected) ->
+            val result = No24723().solve(BufferedReader(given.reader()))
+            result shouldBe expected
+        }
+    }
+})
