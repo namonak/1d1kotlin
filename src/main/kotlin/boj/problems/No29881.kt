43a0442cbf9602d098aaef0dc241c2af5ea43364
namonak <namonak@gmail.com>
2025-12-29 17:03:31 +0900

    feat(boj): add test and production code of No29881 (Data Structure, Set and Map, Hash Set)

---
+++ src/main/kotlin/boj/problems/No29881.kt
@@ -0,0 +1,33 @@
+package boj.problems
+
+import java.io.BufferedReader
+import java.util.StringTokenizer
+
+class No29881 {
+    fun solve(input: BufferedReader): String {
+        val n = input.readLine().trim().toInt()
+
+        val lastNameByFirstName = mutableMapOf<String, String>()
+        repeat(n) {
+            val st = StringTokenizer(input.readLine())
+            val firstName = st.nextToken()
+            val lastName = st.nextToken()
+            lastNameByFirstName[firstName] = lastName
+        }
+
+        val accountNameByFirstName = mutableMapOf<String, String>()
+        repeat(n) {
+            val st = StringTokenizer(input.readLine())
+            val firstName = st.nextToken()
+            val accountName = st.nextToken()
+            accountNameByFirstName[firstName] = accountName
+        }
+
+        return buildString {
+            for ((firstName, lastName) in lastNameByFirstName) {
+                val accountName = accountNameByFirstName[firstName]!!
+                appendLine("$lastName $accountName")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/boj/problems/No29881Test.kt
@@ -0,0 +1,25 @@
+package boj.problems
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class No29881Test : StringSpec({
+    "Account Names : https://www.acmicpc.net/problem/29881" {
+        val given = """
+            3
+            Juku Kask
+            Mann Kuusk
+            Mikk Tamm
+            Mikk mt
+            Juku jk
+            Mann mk
+        """.trimIndent()
+        val expected = """
+            Kask jk
+            Kuusk mk
+            Tamm mt            
+        """.trimIndent()
+
+        No29881().solve(given.reader().buffered()) shouldBe expected
+    }
+})
