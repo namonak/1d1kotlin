452a4b9544499e5b4a3ca3fc718c673a8f88a479
namonak <namonak@gmail.com>
2026-06-10 17:26:57 +0900

    refactor(boj): simplify traversal in No2589

--- src/main/kotlin/boj/problems/No2589.kt
+++ src/main/kotlin/boj/problems/No2589.kt
@@ -42,8 +42,7 @@ class No2589 {
                     val nx = curX + dx
                     val ny = curY + dy
 
-                    if (nx !in map.indices || ny !in map[0].indices) continue
-                    if (map[nx][ny] == 'W' || visited[nx][ny]) continue
+                    if (!isUnvisitedLand(map, visited, nx, ny)) continue
 
                     visited[nx][ny] = true
                     queue.add(nx to ny)
@@ -53,5 +52,14 @@ class No2589 {
         }
 
         return distance
+    }
+
+    private fun isUnvisitedLand(
+        map: Array<CharArray>,
+        visited: Array<BooleanArray>,
+        x: Int,
+        y: Int
+    ): Boolean {
+        return x in map.indices && y in map[0].indices && map[x][y] == 'L' && !visited[x][y]
     }
 }
